0df9ebf4f3eb78ea315a22ba7e42c200fa62912b
Yasaswin Palukuri <p7981595393@gmail.com>
2022-08-31 16:19:43 +0530

    Updating MatrixMultiplication Comments

--- matrixMultiplication.java
+++ matrixMultiplication.java
@@ -4,30 +4,37 @@ public class matrixMultiplication {
     static Scanner scan = new Scanner(System.in);
     public static void main(String args[]) {
         int t = scan.nextInt(); // Initial value of array
-        int n = scan.nextInt();
-        int m = scan.nextInt();
+        int n = scan.nextInt(); // No.of Rows
+        int m = scan.nextInt(); // No.of Columns
             
-        int[][] a = new int[n][m];
-        int[][] b = new int[m][n];
-            
+        int[][] a = new int[n][m]; // Matrix
+        int[][] b = new int[m][n]; // Tranpose Matrix
+        
+        // Creating an matrix and it's transpose with given initial value.
         for(int i=0;i<n;i++) for(int j=1;j<=m;j++){
             a[i][j-1] = t;
             b[j-1][i] = t;
             t++;
         }
         
-        System.out.print("array:\n");
+        // Matrix filled with values with given initial value.
+        System.out.print("Matrix:\n");
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++) System.out.print(a[i][j] + " ");
             System.out.println();
         }
         
-        System.out.print("transpose array:\n");
+        // Tranpose Matrix filled with values with the given initial value.
+        System.out.print("Transpose Matrix:\n");
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++) System.out.print(b[i][j] + " ");
             System.out.println();
         }
         
+
+        // Matrix multiplication
+        // Matrix has nxm => transpose has mxn
+        // After Multiplication matrix will be nxm x mxn => nxn
         int[][] rslt = new int[n][n];
         System.out.println("Multiplication of given two matrices is:");
         int i, j, k;
