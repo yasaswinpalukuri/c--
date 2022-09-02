549c0f67852f504791aa8d2f11359210f41adaf5
Yasaswin Palukuri <p7981595393@gmail.com>
2022-09-02 09:43:03 +0530

    Adding scheduleDeparture.cpp

---
+++ schedueDeparture.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/*
+    You are given "m" timing, that the arrival times of students at the bus stop, We have "n" cars with each capacity 5.
+    we need to schedule the departure of the cars in such a way that the wait time is minimal.
+    Given the cars, takes no time to reach the bus stop
+
+    Print the depart times of the cars with optimal distribution and also print the wait time for the students.
+
+*/
+int main(){
+    
+}
