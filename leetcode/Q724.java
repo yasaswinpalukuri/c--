e2eaf9ad6bcd1f2ad12587f6875619e99c7bd98a
Yasaswin Palukuri <p7981595393@gmail.com>
2022-09-13 08:33:08 +0530

    Updating Q724

--- leetcode/Q724.java
+++ leetcode/Q724.java
@@ -8,7 +8,8 @@ public class Q724 {
         int n = scan.nextInt();
         int a[] = new int[n];
         for(int i=0;i<n;i++) a[i] = scan.nextInt();
-        System.out.println(pivotIndex(a));
+        System.out.println("pivotIndex() : "+pivotIndex(a));
+        System.out.println("pivotIndex2() : "+pivotIndex2(a));
     }
 
     public static int pivotIndex(int[] nums) {
@@ -18,6 +19,16 @@ public class Q724 {
         for(int i=0;i<nums.length;i++){
             if(leftSum == sum - leftSum - nums[i]) return i;
             leftSum += nums[i];
+        }
+        return -1;
+    }
+
+    public static int pivotIndex2(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            int l=0,r=0;
+            for(int j=0;j<i;j++) l += nums[j];
+            for(int j=i+1;j<nums.length;j++) r += nums[j];
+            if(l == r) return i;
         }
         return -1;
     }
