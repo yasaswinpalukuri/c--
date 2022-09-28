e1b338dfb6cff93d912f46d6448d844fac217bec
Yasaswin Palukuri <p7981595393@gmail.com>
2022-09-28 15:34:24 +0530

    Adding capgemini.java

---
+++ coding/capgemini.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class capgemini {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = scan.nextInt();
+        System.out.println(n);
+    }
+}
