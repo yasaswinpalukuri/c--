c391894b725fea0bc4b6ffcdee2004d0d9985319
Yasaswin Palukuri <p7981595393@gmail.com>
2022-12-06 14:05:16 +0530

    Updating wiley.java

--- coding/wiley.java
+++ coding/wiley.java
@@ -2,34 +2,33 @@ import java.util.*;
 
 public class wiley {
     static Scanner scan = new Scanner(System.in);
-    // public static void main(String args[]){
-    //     int n = scan.nextInt();
-    //     for(int i=1;i<=n;i++){
-    //         int j = 1;
-    //         for(;j<i;j++){
-    //             System.out.print(" ");
-    //         }
-    //         System.out.println(i);
-    //         for(j=1;j<n;j++){
-    //             System.out.print(" ");
-    //         }
-    //     }
-    // }    
-
     public static void main(String args[]){
-        String s = scan.nextLine();
-        int n = s.length();
-        // System.out.println(s + " " + n);
-        if(n > 100) System.out.println("-1");
-        else if(Character.isDigit(s.charAt(n-1))){
-            // System.out.println("Yes last character is digit");
-            if(s.charAt(n-1) == (n/10)+'0') System.out.println((n-1)%10);
-            else System.out.println(-1);
+        int n = scan.nextInt();
+        int[][] arr = new int[2*n -1][2*n -1];
+        for(int i=0;i<2*n-1;i++) arr[i][i] = arr[i][2*n-2-i] = (i+1 > n) ? 2*n - i - 1 : i+1;
+        for(int i=0;i<2*n-1;i++){
+            for(int j=0;j<2*n-1;j++){
+                if(arr[i][j] != 0) System.out.print(arr[i][j]+" ");
+                else System.out.print("  ");
+            }
+            System.out.println();
         }
-        else{
-            // System.out.println("No last character is not digit");
-            if(n > 9) System.out.println(-1); 
-            else System.out.println(n);
-        }
-    }
+    }    
+
+    // public static void main(String args[]){
+    //     String s = scan.nextLine();
+    //     int n = s.length();
+    //     // System.out.println(s + " " + n);
+    //     if(n > 100) System.out.println("-1");
+    //     else if(Character.isDigit(s.charAt(n-1))){
+    //         // System.out.println("Yes last character is digit");
+    //         if(s.charAt(n-1) == (n/10)+'0') System.out.println((n-1)%10);
+    //         else System.out.println(-1);
+    //     }
+    //     else{
+    //         // System.out.println("No last character is not digit");
+    //         if(n > 9) System.out.println(-1); 
+    //         else System.out.println(n);
+    //     }
+    // }
 }
