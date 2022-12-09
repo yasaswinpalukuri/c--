536cbf6abc04934fa22d122786eafb2933c2d2e0
Yasaswin Palukuri <p7981595393@gmail.com>
2022-12-09 19:44:45 +0530

    Udpating wiley.java

--- coding/wiley.java
+++ coding/wiley.java
@@ -15,20 +15,20 @@ public class wiley {
         }
     }    
 
-    // public static void main(String args[]){
-    //     String s = scan.nextLine();
-    //     int n = s.length();
-    //     // System.out.println(s + " " + n);
-    //     if(n > 100) System.out.println("-1");
-    //     else if(Character.isDigit(s.charAt(n-1))){
-    //         // System.out.println("Yes last character is digit");
-    //         if(s.charAt(n-1) == (n/10)+'0') System.out.println((n-1)%10);
-    //         else System.out.println(-1);
-    //     }
-    //     else{
-    //         // System.out.println("No last character is not digit");
-    //         if(n > 9) System.out.println(-1); 
-    //         else System.out.println(n);
-    //     }
-    // }
+    public static void stringCheck(){
+        String s = scan.nextLine();
+        int n = s.length();
+        // System.out.println(s + " " + n);
+        if(n > 100) System.out.println("-1");
+        else if(Character.isDigit(s.charAt(n-1))){
+            // System.out.println("Yes last character is digit");
+            if(s.charAt(n-1) == (n/10)+'0') System.out.println((n-1)%10);
+            else System.out.println(-1);
+        }
+        else{
+            // System.out.println("No last character is not digit");
+            if(n > 9) System.out.println(-1); 
+            else System.out.println(n);
+        }
+    }
 }
