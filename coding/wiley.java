cc3d87a8e62479b56527c4a0216c9455fe6ea40f
Yasaswin Palukuri <p7981595393@gmail.com>
2022-12-09 19:48:57 +0530

    updating wiley.java

--- coding/wiley.java
+++ coding/wiley.java
@@ -3,6 +3,11 @@ import java.util.*;
 public class wiley {
     static Scanner scan = new Scanner(System.in);
     public static void main(String args[]){
+        // printPattern();
+        // changeUnreadableChars();
+        stringCheck();
+    }
+    public static void printPattern(){
         int n = scan.nextInt();
         int[][] arr = new int[2*n -1][2*n -1];
         for(int i=0;i<2*n-1;i++) arr[i][i] = arr[i][2*n-2-i] = (i+1 > n) ? 2*n - i - 1 : i+1;
@@ -14,6 +19,22 @@ public class wiley {
             System.out.println();
         }
     }    
+
+    public static void changeUnreadableChars(){
+        String s1 = scan.next();
+        String s2 = scan.next();
+        int max=0,min=0;
+        for(int j=0;j<s1.length();j++){
+            if(s1.charAt(j)!=s2.charAt(j) && s1.charAt(j)!='?' && s2.charAt(j)!='?'){
+                min++;
+                max++;
+            }
+            else if(s1.charAt(j)=='?' || s2.charAt(j)=='?'){
+                max++;
+            }
+        }
+        System.out.println(min+" "+max);
+    }
 
     public static void stringCheck(){
         String s = scan.nextLine();
