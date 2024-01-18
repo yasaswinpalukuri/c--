2c87243252166e33e2a4956e124595c9c90b99c2
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-01-18 10:22:36 -0500

    ADDDING DATA ANNOTATION QUALIFICATION QUESTION

---
+++ coding/datannotation.py
@@ -0,0 +1,31 @@
+def decode(message_file):
+    # Read the content of the file
+    with open(message_file, 'r') as file:
+        lines = file.readlines()
+
+    # Initialize variables
+    pyramid = []
+    decoded_words = []
+
+    # Process each line in the file
+    for line in lines:
+        # Split each line into number and word
+        parts = line.split()
+        number, word = int(parts[0]), parts[1]
+
+        # Add the word to the pyramid list
+        pyramid.append((number, word))
+
+        # If the pyramid is complete, decode the message
+        if len(pyramid) == pyramid[-1][0]:
+            decoded_words.append(pyramid[-1][1])
+            pyramid = []
+
+    # Combine the decoded words into a string
+    decoded_message = ' '.join(decoded_words)
+    return decoded_message
+
+# Example usage
+message_file = 'A:\my repo\c--\coding\coding_qual_input.txt'
+result = decode(message_file)
+print(result)
