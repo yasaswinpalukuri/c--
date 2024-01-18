f058b4ffa96010c7c63d2a09a3e27c6ade16e7d5
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-01-18 10:57:17 -0500

    updating setting

---
+++ coding/dataAnnotation.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<fstream>
+using namespace std;
+
+
+int main(){
+
+    ifstream MyReadFile("A:\my repo\c--\coding\coding_qual_input.txt");
+    string read;
+    while (getline(MyReadFile, read)){
+        cout << read << "\n";
+        insertWord(r,read);
+    }
+    MyReadFile.close();
+
+}
