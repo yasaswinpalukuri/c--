93faea4a0e0b7779a6c922fda9c4b52b26d0d2bf
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-01-20 23:43:03 -0500

    Finalizing the code and updating file

--- coding/dataAnnotation.cpp
+++ coding/dataAnnotation.cpp
@@ -5,13 +5,15 @@ using namespace std;
 
 int main(){
 
-    ifstream MyReadFile("A:\my repo\c--\coding\coding_qual_input.txt");
-    string read;
-    while (getline(MyReadFile, read)){
-        cout << read << "\n";
-        insertWord(r,read);
-    }
-    MyReadFile.close();
+    // ifstream MyReadFile("A:\my repo\c--\coding\coding_qual_input.txt");
+    // string read;
+    // while (getline(MyReadFile, read)){
+    //     cout << read << "\n";
+    //     insertWord(r,read);
+    // }
+    // MyReadFile.close();
 
+    int n = 24;
+    cout<<n;
     return 0;
 }
--- coding/datannotation.py
+++ coding/datannotation.py
@@ -1,46 +1,41 @@
-def decode(message_file):
-    # Read the content of the file
+def decode_with_numbers(message_file):
+    print("Data annotation assignment:")
     with open(message_file, 'r') as file:
         lines = file.readlines()
 
-    # Initialize variables
     pyramid = []
-    decoded_words = []
+    decoded_sequence = []
 
-    # Process each line in the file
     for line in lines:
-        # Split each line into number and word
         parts = line.split()
         number, word = int(parts[0]), parts[1]
 
-        # Add the word to the pyramid list
         pyramid.append((number, word))
+        # print(pyramid)
 
-        # If the pyramid is complete, decode the message
         if len(pyramid) == pyramid[-1][0]:
-            decoded_words.append(pyramid[-1][1])
-            print("decodeed:"+decoded_message)
+            decoded_sequence.extend(pyramid)
             pyramid = []
-
-    # Combine the decoded words into a string
-    decoded_message = ' '.join(decoded_words)
-    return decoded_message
-
-# Example usage
-message_file = 'A:\my repo\c--\coding\coding_qual_input.txt'
-result = decode(message_file)
-print(result)
-
-
-
-"""
-           1
-      /         \
-     2           3
-   /   \       /   \
-  4     5     6     7
- / \   / \   / \   / \
-8  9  10 11 12 13 14 15
     
-1,3,7,15
-"""
+    # print("Pyramid:"+ str(pyramid))
+    print("Decode sequence:"+ str(decoded_sequence))
+
+"""def decode_with_numbers(message_file):
+    with open(message_file, 'r') as file:
+        lines = file.readlines()
+
+    decoded_sequence = []
+    current_position = 1
+
+    for line in lines:
+        parts = line.split()
+        number, word = int(parts[0]), parts[1]
+
+        if number == current_position:
+            decoded_sequence.append((number, word))
+            current_position += 1
+
+    print(decoded_sequence)"""
+# Example usage
+message_file = 'A:\my repo\c--\coding\dataAnnotationTextFile.txt'
+decode_with_numbers(message_file)
