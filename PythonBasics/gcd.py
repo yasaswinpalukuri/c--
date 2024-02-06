07eead57c7e0679fb2a3e2580172d03640ba347d
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-02-06 18:37:47 -0500

    Updating gcd.py

--- PythonBasics/gcd.py
+++ PythonBasics/gcd.py
@@ -4,10 +4,6 @@ def GCD(a,b):
     else:
         return GCD(b,a%b)
 
-def main():
-    a = int(input("Enter the first number: "))
-    b = int(input("Enter the second number: "))
-    print("The GCD of",a,"and",b,"is",GCD(a,b))
-
-if __name__ == "__main__":
-    main()
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("The GCD of",a,"and",b,"is",GCD(a,b))
