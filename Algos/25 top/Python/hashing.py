ab1b1e44ec996c3f7d115c61c3c910c2eaee0fc0
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-02-29 11:53:45 -0500

    Updating hashing.py

--- Algos/25 top/Python/hashing.py
+++ Algos/25 top/Python/hashing.py
@@ -49,34 +49,41 @@ Hashing
         => Load balancing
 '''
 
-def hashFunction(key, size):
-    return key % size
+# Function to display hashtable 
+def display_hash(hashTable): 
+	
+	for i in range(len(hashTable)): 
+		print(i, end = " ") 
+		
+		for j in hashTable[i]: 
+			print("-->", end = " ") 
+			print(j, end = " ") 
+			
+		print() 
 
-class HashTable:
-    def __init__(self):
-        self.MAX = 10
-        self.arr = [None for i in range(self.MAX)]
-    
-    def get_hash(self, key):
-        return key % self.MAX
-    
-    def __setitem__(self, key, value):
-        h = self.get_hash(key)
-        self.arr[h] = value
-    
-    def __getitem__(self, key):
-        h = self.get_hash(key)
-        return self.arr[h]
-    
-    def __delitem__(self, key):
-        h = self.get_hash(key)
-        self.arr[h] = None
+# Creating Hashtable as 
+# a nested list. 
+HashTable = [[] for _ in range(10)] 
 
-t = HashTable()
-t[23] = "apple"
-t[25] = "mango"
-t[27] = "banana"
-print(t.arr)
-print(t[25])
-del t[25]
-print(t.arr)
+# Hashing Function to return 
+# key for every value. 
+def Hashing(keyvalue): 
+	return keyvalue % len(HashTable) 
+
+
+# Insert Function to add 
+# values to the hash table 
+def insert(Hashtable, keyvalue, value): 
+	
+	hash_key = Hashing(keyvalue) 
+	Hashtable[hash_key].append(value) 
+
+# Driver Code 
+insert(HashTable, 10, 'Allahabad') 
+insert(HashTable, 25, 'Mumbai') 
+insert(HashTable, 20, 'Mathura') 
+insert(HashTable, 9, 'Delhi') 
+insert(HashTable, 21, 'Punjab') 
+insert(HashTable, 21, 'Noida') 
+
+display_hash (HashTable) 
