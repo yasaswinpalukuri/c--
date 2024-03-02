620ec982be865455a3ffb1e53f6f97bffe183899
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-03-02 07:51:03 -0500

    Updating AStar.py

--- Algos/25 top/Python/AStar.py
+++ Algos/25 top/Python/AStar.py
@@ -26,18 +26,18 @@ A* algorithm
 	-> Limitations:
 	    => It is not guaranteed to find the shortest path
         
-	Summary 
-    So when to use BFS over A*, when to use Dijkstra over A* to find the shortest paths ? 
-    We can summarise this as below-
-    1) One source and One Destination- 
-    ? Use A* Search Algorithm (For Unweighted as well as Weighted Graphs)
-    2) One Source, All Destination – 
-    ? Use BFS (For Unweighted Graphs) 
-    ? Use Dijkstra (For Weighted Graphs without negative weights) 
-    ? Use Bellman Ford (For Weighted Graphs with negative weights)
-    3) Between every pair of nodes- 
-    ? Floyd-Warshall 
-    ? Johnson’s Algorithm
+	-> Summary 
+        => So when to use BFS over A*, when to use Dijkstra over A* to find the shortest paths ? 
+        => We can summarise this as below-
+            1) One source and One Destination- 
+                ? Use A* Search Algorithm (For Unweighted as well as Weighted Graphs)
+            2) One Source, All Destination 
+                ? Use BFS (For Unweighted Graphs) 
+                ? Use Dijkstra (For Weighted Graphs without negative weights) 
+                ? Use Bellman Ford (For Weighted Graphs with negative weights)
+            3) Between every pair of nodes- 
+                ? Floyd-Warshall 
+                ? Johnson's Algorithm
 '''
 
 import math
