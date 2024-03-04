b86ff3160a6793f0c5e22ec370ae230c2f98fcd2
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-03-03 23:26:58 -0500

    Updating dijkstra.py

---
+++ Algos/25 top/Python/dijkstra.py
@@ -0,0 +1,16 @@
+'''
+Dijkstra's Algorithm
+    -> Finds shortest path btw source and all nodes.
+    -> Weights are used to verify the shortest path.
+    -> Creates a route with all nodes with possible shortest path.
+
+    -> Limitations:
+        => Works with only positive weights.
+        => 
+
+    -> Uses:
+        => GPS Devices
+        => Network Routing Protocols
+        => Airline Traffic Control
+
+'''
