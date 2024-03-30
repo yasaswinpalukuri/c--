fe9769eb0712dd1873b3cd0f7cb64ceeeb3430a5
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-03-30 00:57:11 -0400

    Updating dataAnnotation.cpp

--- coding/dataAnnotation.cpp
+++ coding/dataAnnotation.cpp
@@ -5,13 +5,22 @@ using namespace std;
 
 int main(){
 
-    // ifstream MyReadFile("A:\my repo\c--\coding\coding_qual_input.txt");
-    // string read;
-    // while (getline(MyReadFile, read)){
-    //     cout << read << "\n";
-    //     insertWord(r,read);
-    // }
-    // MyReadFile.close();
+    ifstream MyReadFile("dataAnnotationTextFile.txt");
+    string read;
+    int key; string value;
+    while (getline(MyReadFile, read)){
+        // if (sscanf(read.c_str(), "%d %s", &key, &value[0]) == 2) cout << "Key : " << key << ", Value: " << value << endl;
+        // cout << read << "\n";
+
+        stringstream iss(read);
+        if (iss >> key >> value) {
+            // Printing in the desired format
+            cout << "Key : " << key << ", Value:" << value << endl;
+        } else {
+            cerr << "Error: Invalid line format" << endl;
+        }
+    }
+    MyReadFile.close();
 
     int n = 24;
     cout<<n;
