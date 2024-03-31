1ddc047e8fb5c19cfed4605bd38135a800aec1e9
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-03-31 02:28:42 -0400

    Updating staircase.py

--- coding/staircase.py
+++ coding/staircase.py
@@ -28,3 +28,6 @@ def create_staircase1(nums):
 l = [1,2,3,4,5,6,7]
 print(create_staircase(l))
 print(create_staircase1(l))
+
+print(create_staircase([1,2,3,4,5,6]))
+print(create_staircase1([1,2,3,4,5,6]))
