ea2d1c0f10b038d9b4acda26720dbaad4a38ee90
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-03-31 02:16:41 -0400

    Adding staircase.py

---
+++ coding/staircase.py
@@ -0,0 +1,30 @@
+def create_staircase(nums):
+  step = 1
+  subsets = []
+  while len(nums) != 0:
+    if len(nums) >= step:
+      subsets.append(nums[0:step])
+      nums = nums[step:]
+      step += 1
+    else:
+      return False
+      
+  return subsets
+
+def create_staircase1(nums):
+  while len(nums) != 0:
+    step = 1
+    subsets = []
+    if len(nums) >= step:
+      subsets.append(nums[0:step])
+      nums = nums[step:]
+      step += 1
+    else:
+      return False
+
+  return subsets
+
+
+l = [1,2,3,4,5,6,7]
+print(create_staircase(l))
+print(create_staircase1(l))
