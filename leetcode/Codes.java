8c9533d1a9ed6c1654b06de966de74e4b0bc775b
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-04-22 21:13:07 -0400

    Updating codes.java

--- leetcode/Codes.java
+++ leetcode/Codes.java
@@ -5,7 +5,37 @@ import java.io.*;
 public class Codes {
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        do{
+            System.out.println("----MENU----");
+            System.out.println("1. Valid Path");
+            System.out.println("2. Exit");
+            System.out.print("Enter your choice: ");
+            int ch = scan.nextInt();
+            switch(ch){
+                case 1:
+                    System.out.print("Enter the number of nodes: ");
+                    int n = scan.nextInt();
+                    System.out.print("Enter the number of edges: ");
+                    int m = scan.nextInt();
+                    int[][] edges = new int[m][2];
+                    for(int i=0; i<m; i++){
+                        System.out.print("Enter the edge " + (i+1) + ": ");
+                        edges[i][0] = scan.nextInt();
+                        edges[i][1] = scan.nextInt();
+                    }
+                    System.out.print("Enter the source node: ");
+                    int source = scan.nextInt();
+                    System.out.print("Enter the destination node: ");
+                    int destination = scan.nextInt();
+                    System.out.println(validPath(n, edges, source, destination));
+                    break;
+                case 2:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }while(true);
     }
     static public boolean validPath(int n, int[][] edges, int source, int destination) {
         if(edges.length == 0) return true;
