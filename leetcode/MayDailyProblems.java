00221ac04acd86106da37ab8802537139923a4cb
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-04-29 15:56:24 -0400

    Changed the name of the file according to the context of the file.

--- leetcode/MayDailyProblems.java
+++ leetcode/MayDailyProblems.java
@@ -2,7 +2,7 @@ package leetcode;
 import java.util.*;
 
 // This File consists of all the problems solved in May Daily Leetcode Challenge 2024
-public class MayChallenge {
+public class MayDailyProblems {
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
         
