adbb3a8ea8249e32259ed87feb6c85610d5c2450
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-04-29 15:51:06 -0400

    Adding the May Daily Challenges file

--- leetcode/MayChallenge.java
+++ leetcode/MayChallenge.java
@@ -1,5 +1,10 @@
 package leetcode;
+import java.util.*;
 
+// This File consists of all the problems solved in May Daily Leetcode Challenge 2024
 public class MayChallenge {
-    
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    }
 }
