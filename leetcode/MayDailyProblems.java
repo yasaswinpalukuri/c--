f88f8994beb545f515ab5730b2a7d9b9756ec5f6
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-04-29 16:26:31 -0400

    Adding structure to MayDailyProblems file

--- leetcode/MayDailyProblems.java
+++ leetcode/MayDailyProblems.java
@@ -5,6 +5,17 @@ import java.util.*;
 public class MayDailyProblems {
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        System.out.println("Welcome to May Daily Leetcode Problems");
+        System.out.println("This is Yasaswin Palukuri, with solutions to the Daily Problems of May 2024");
+        System.out.println("So, without any further ado, let's get started");
+        do{
+            System.out.println("Enter the day of the problem you want the answer for:");
+            int day = scan.nextInt();
+            switch(day){
+                default:
+                    System.out.println("Sorry, the problem for the day you entered is not available");
+                    break;
+            }
+        }while(true);
     }
 }
