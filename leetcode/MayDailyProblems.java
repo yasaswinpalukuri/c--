4053102bfde8b7518b31877490784dd1d07faf5c
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-01 22:41:13 -0400

    Added statement for day 2

--- leetcode/MayDailyProblems.java
+++ leetcode/MayDailyProblems.java
@@ -70,4 +70,23 @@ public class MayDailyProblems {
         
         return sb.toString();
     }
+
+    // 2441 - Largest Positive Integer That Exists With Its Negative - Easy
+    /*
+    Example 1:
+    Input: nums = [-1,2,-3,3]
+    Output: 3
+    Explanation: 3 is the only valid k we can find in the array.
+   
+    Example 2:
+    Input: nums = [-1,10,6,7,-7,1]
+    Output: 7
+    Explanation: Both 1 and 7 have their corresponding negative values in the array. 7 has a larger value.
+    
+    Example 3:
+    Input: nums = [-10,8,6,7,-2,-3]
+    Output: -1
+    Explanation: There is no a single valid k, we return -1.
+    */
+    
 }
