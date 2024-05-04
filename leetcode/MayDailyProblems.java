65c4ee155e206bc3af632260274986696c2af98b
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-03 20:20:26 -0400

    Adding details of Day 4: Boats to Save People to java file

--- leetcode/MayDailyProblems.java
+++ leetcode/MayDailyProblems.java
@@ -185,4 +185,22 @@ public class MayDailyProblems {
         }
         return 0;
     }
+
+    // Day 4: Boats to Save People - Q881(Medium)
+    /*
+    Example 1:
+    Input: people = [1,2], limit = 3
+    Output: 1
+    Explanation: 1 boat (1, 2)
+    
+    Example 2:
+    Input: people = [3,2,2,1], limit = 3
+    Output: 3
+    Explanation: 3 boats (1, 2), (2) and (3)
+    
+    Example 3:
+    Input: people = [3,5,3,4], limit = 5
+    Output: 4
+    Explanation: 4 boats (3), (3), (4), (5)
+    */
 }
