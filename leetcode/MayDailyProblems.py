6950221feffba03a136f045beae4c8496bbd505e
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-04 22:41:19 -0400

    Updating comments and printing statement in MayDailyProblems python file.

--- leetcode/MayDailyProblems.py
+++ leetcode/MayDailyProblems.py
@@ -145,6 +145,22 @@ class Solutions:
             boats += 1
         return boats
 
+    # Day 5: Delete Node in a Linked List - Q237(Easy)
+    '''    
+    Example 1:
+    Input: head = [4,5,1,9], node = 5
+    Output: [4,1,9]
+    Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+
+    Example 2:
+    Input: head = [4,5,1,9], node = 1
+    Output: [4,5,9]
+    Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+    '''
+    def deleteNode(self, node):
+        node.val = node.next.val
+        node.next = node.next.next
+
 
 def main():
     print("Welcome to May Daily Leetcode Problems")
