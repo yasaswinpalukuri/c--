cd1094ce4a78c1511f9f32bf303e5c371e915c2e
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-05 09:18:46 -0400

    Added minimum path sum an medium level, question that greatly integrates, dp and backtrcking. Asked and depicted in many l2 interview rounds to understand the formulation of the participants minds and their approaches.

--- leetcode/Codes.java
+++ leetcode/Codes.java
@@ -45,6 +45,7 @@ public class Codes {
             System.out.println("15. Island Perimeter");
             System.out.println("16. Smallest String Starting From Leaf");
             System.out.println("17. Add One Row to Tree");
+            System.out.println("18. Minimum Path Sum");
             System.out.println("8888. Exit");
             System.out.print("Enter your choice: ");
             int ch = scan.nextInt();
@@ -235,6 +236,20 @@ public class Codes {
                     System.out.print("Enter the depth: ");
                     int depth = scan.nextInt();
                     System.out.println(addOneRow(root, val, depth));
+                    break;
+                case 18:
+                    System.out.print("Enter the number of rows: ");
+                    int rowsSum = scan.nextInt();
+                    System.out.print("Enter the number of columns: ");
+                    int colsSum = scan.nextInt();
+                    int[][] gridSum = new int[rowsSum][colsSum];
+                    for(int i=0; i<rowsSum; i++){
+                        for(int j=0; j<colsSum; j++){
+                            System.out.print("Enter the value at " + i + ", " + j + ": ");
+                            gridSum[i][j] = scan.nextInt();
+                        }
+                    }
+                    System.out.println("So, the Minimum Path Sum from (0,0) to (m-1,n-1) is: "+minPathSum(gridSum));
                     break;
                 case 8888:
                     System.exit(0);
@@ -798,5 +813,37 @@ public class Codes {
         addOneRow(root.left,val,depth-1);
         addOneRow(root.right,val,depth-1);
         return root;
+    }
+
+    // Minimum Path Sum - Q64(Medium)
+    /*
+    need to go from (0,0) to (m-1,n-1) in a grid and find the minimum path sum
+
+    Example 1:
+    Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+    Output: 7
+    Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+
+    Example 2:
+    Input: grid = [[1,2,3],[4,5,6]]
+    Output: 12
+    */
+    static public int minPathSum(int[][] grid){
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for(int i=1; i<m; i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for(int i=1; i<n; i++){
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
