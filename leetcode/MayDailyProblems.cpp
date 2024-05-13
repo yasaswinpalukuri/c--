779ff529e4c4cbf744cedccd88e7f6c72c8298ff
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-13 08:54:54 -0400

    Added solution and comments to the cpp file for Day 13

--- leetcode/MayDailyProblems.cpp
+++ leetcode/MayDailyProblems.cpp
@@ -466,6 +466,21 @@ class Solutions{
     Output: 1
     */
     int matrixScore(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int score = (1 << (n - 1)) * m;
+        for (int j = 1; j < n; j++) {
+            int countSameBits = 0;
+            for (int i = 0; i < m; i++) {
+                if (grid[i][j] == grid[i][0]) {
+                    countSameBits++;
+                }
+            }
+            countSameBits = max(countSameBits, m - countSameBits);
+            int columnScore = (1 << (n - j - 1)) * countSameBits;
+            score += columnScore;
+        }
+        return score;
     }
 };
 
