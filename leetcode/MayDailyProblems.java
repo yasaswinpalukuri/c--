017adff66fd471dcd0b014b02f26e151c70556c2
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-14 07:21:37 -0400

    Added solution and comments to the java file for Day 14

--- leetcode/MayDailyProblems.java
+++ leetcode/MayDailyProblems.java
@@ -203,6 +203,7 @@ public class MayDailyProblems {
                         }
                     }
                     System.out.println("The maximum gold that can be obtained is: " + getMaximumGold(grid2));
+                    break;
                 case 88:
                     System.out.println("Thank you for using the May Daily Leetcode Problems :)");
                     System.exit(0);
@@ -692,7 +693,32 @@ public class MayDailyProblems {
     [9,0,20]]
     Path to get the maximum gold, 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7.
     */
+    static final int[] DIRECTIONS = new int[] { 0, 1, 0, -1, 0 };
+
     static public int getMaximumGold(int[][] grid) {
-        return -1;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int maxGold = 0;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                maxGold = Math.max(maxGold, dfsBacktrack(grid, rows, cols, row, col));
+            }
+        }
+        return maxGold;
+    }
+
+    static private int dfsBacktrack(int[][] grid, int rows, int cols, int row, int col){
+        if (row < 0 || col < 0 || row == rows || col == cols || grid[row][col] == 0) {
+            return 0;
+        }
+        int maxGold = 0;
+        int originalVal = grid[row][col];
+        grid[row][col] = 0;
+        for (int direction = 0; direction < 4; direction++) {
+            maxGold = Math.max(maxGold,
+                    dfsBacktrack(grid, rows, cols, DIRECTIONS[direction] + row,DIRECTIONS[direction + 1] + col));
+        }
+        grid[row][col] = originalVal;
+        return maxGold + originalVal;
     }
 }
