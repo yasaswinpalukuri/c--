d196b536d182545da3fea8a5704b010ec5c60059
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-14 07:25:26 -0400

    Added solution and comments to the python file for Day 14

--- leetcode/MayDailyProblems.py
+++ leetcode/MayDailyProblems.py
@@ -425,6 +425,29 @@ class Solutions:
     [9,0,20]]
     Path to get the maximum gold, 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7.
     '''
+    def getMaximumGold(self, grid: list[list[int]]) -> int:
+        DIRECTIONS = [0, 1, 0, -1, 0]
+        rows = len(grid)
+        cols = len(grid[0])
+        max_gold = 0
+
+        def dfs_backtrack(grid, rows, cols, row, col):
+            if row < 0 or col < 0 or row == rows or col == cols or \
+                    grid[row][col] == 0:
+                return 0
+            max_gold = 0
+            original_val = grid[row][col]
+            grid[row][col] = 0
+            for direction in range(4):
+                max_gold = max(max_gold,dfs_backtrack(grid, rows, cols, DIRECTIONS[direction] + row,DIRECTIONS[direction + 1] + col))
+
+            grid[row][col] = original_val
+            return max_gold + original_val
+
+        for row in range(rows):
+            for col in range(cols):
+                max_gold = max(max_gold, dfs_backtrack(grid, rows, cols, row, col))
+        return max_gold
 
 def main():
     print("Welcome to May Daily Leetcode Problems")
