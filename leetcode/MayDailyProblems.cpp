99f824d2e23d81ec56cabba2966954c43485a56c
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-15 08:40:28 -0400

    Added solution and comments to the cpp file for Day 15

--- leetcode/MayDailyProblems.cpp
+++ leetcode/MayDailyProblems.cpp
@@ -556,6 +556,89 @@ class Solutions{
     - The closest cell of the path to the thief at cell (3, 0) is cell (3, 2). The distance between them is | 3 - 3 | + | 0 - 2 | = 2.
     It can be shown that there are no other paths with a higher safeness factor.
     */
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        int n = grid.size();
+        queue<pair<int, int>> multiSourceQueue;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1){
+                    multiSourceQueue.push({i, j});
+                    grid[i][j] = 0;
+                } else {
+                    grid[i][j] = -1;
+                }
+            }
+        }
+        while (!multiSourceQueue.empty()) {
+            int size = multiSourceQueue.size();
+            while (size-- > 0) {
+                auto curr = multiSourceQueue.front();
+                multiSourceQueue.pop();
+                for (auto& d : dir) {
+                    int di = curr.first + d[0];
+                    int dj = curr.second + d[1];
+                    int val = grid[curr.first][curr.second];
+                    if (isValidCell(grid, di, dj) && grid[di][dj] == -1){
+                        grid[di][dj] = val + 1;
+                        multiSourceQueue.push({di, dj});
+                    }
+                }
+            }
+        }
+        int start = 0;
+        int end = 0;
+        int res = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++){
+                end = max(end, grid[i][j]);
+            }
+        }
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (isValidSafeness(grid, mid)){
+                res = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return res;
+    }
+    
+    vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    bool isValidCell(vector<vector<int>>& grid, int i, int j) {
+        int n = grid.size();
+        return i >= 0 && j >= 0 && i < n && j < n;
+    }
+    bool isValidSafeness(vector<vector<int>>& grid, int minSafeness) {
+        int n = grid.size();
+        if (grid[0][0] < minSafeness || grid[n - 1][n - 1] < minSafeness) {
+            return false;
+        }
+
+        queue<pair<int, int>> traversalQueue;
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        traversalQueue.push({0, 0});
+        visited[0][0] = true;
+        while (!traversalQueue.empty()) {
+            auto curr = traversalQueue.front();
+            traversalQueue.pop();
+            if (curr.first == n - 1 && curr.second == n - 1) {
+                return true;
+            }
+            for (auto& d : dir) {
+                int di = curr.first + d[0];
+                int dj = curr.second + d[1];
+                if (isValidCell(grid, di, dj) && !visited[di][dj] &&
+                    grid[di][dj] >= minSafeness) {
+                    visited[di][dj] = true;
+                    traversalQueue.push({di, dj});
+                }
+            }
+        }
+        return false;
+    }
 };
 
 int main() {
@@ -787,7 +870,7 @@ int main() {
                         cin >> grid[i][j];
                     }
                 }
-                cout << "The safest path in the grid is: " << sol.findSafestPath(grid) << '\n';
+                cout << "The safest path in the grid is: " << sol.maximumSafenessFactor(grid) << '\n';
                 break;
             }
             case 88:
