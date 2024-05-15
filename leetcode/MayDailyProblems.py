ab5a9f689e37f087bec019587c6ce82d78e007b7
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-15 09:01:31 -0400

    Added solution and comments to the python file for Day 15

--- leetcode/MayDailyProblems.py
+++ leetcode/MayDailyProblems.py
@@ -473,6 +473,73 @@ class Solutions:
     - The closest cell of the path to the thief at cell (3, 0) is cell (3, 2). The distance between them is | 3 - 3 | + | 0 - 2 | = 2.
     It can be shown that there are no other paths with a higher safeness factor.
     '''
+    dir = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    def maximumSafenessFactor(self, grid: list[list[int]]) -> int:
+        n = len(grid)
+
+        multi_source_queue = deque()
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    multi_source_queue.append((i, j))
+                    grid[i][j] = 0
+                else:
+                    grid[i][j] = -1
+
+        while multi_source_queue:
+            size = len(multi_source_queue)
+            while size > 0:
+                curr = multi_source_queue.popleft()
+                for d in self.dir:
+                    di, dj = curr[0] + d[0], curr[1] + d[1]
+                    val = grid[curr[0]][curr[1]]
+                    if self.isValidCell(grid, di, dj) and grid[di][dj] == -1:
+                        grid[di][dj] = val + 1
+                        multi_source_queue.append((di, dj))
+                size -= 1
+
+        start, end, res = 0, 0, -1
+        for i in range(n):
+            for j in range(n):
+                end = max(end, grid[i][j])
+
+        while start <= end:
+            mid = start + (end - start) // 2
+            if self.isValidSafeness(grid, mid):
+                res = mid
+                start = mid + 1
+            else:
+                end = mid - 1
+
+        return res
+    
+    def isValidCell(self, grid, i, j) -> bool:
+        n = len(grid)
+        return 0 <= i < n and 0 <= j < n
+
+    def isValidSafeness(self, grid, min_safeness) -> bool:
+        n = len(grid)
+
+        if grid[0][0] < min_safeness or grid[n - 1][n - 1] < min_safeness:
+            return False
+
+        traversal_queue = deque([(0, 0)])
+        visited = [[False] * n for _ in range(n)]
+        visited[0][0] = True
+
+        while traversal_queue:
+            curr = traversal_queue.popleft()
+            if curr[0] == n - 1 and curr[1] == n - 1:
+                return True
+
+            for d in self.dir:
+                di, dj = curr[0] + d[0], curr[1] + d[1]
+                if self.isValidCell(grid, di, dj) and not visited[di][dj] and grid[di][dj] >= min_safeness:
+                    visited[di][dj] = True
+                    traversal_queue.append((di, dj))
+
+        return False
 
 
 def main():
@@ -622,7 +689,7 @@ def main():
             print("Enter the elements of the grid row by row:")
             for _ in range(n):
                 grid.append(list(map(int, input().split())))
-            print("The safest path in the grid is:", sol.findSafestPath(grid))
+            print("The safest path in the grid is:", sol.maximumSafenessFactor(grid))
         else:
             print("Sorry, the problem for the day you entered is not available")
 
