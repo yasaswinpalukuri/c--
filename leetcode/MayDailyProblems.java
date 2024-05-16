e72466825beb6d7d82b14c6b69075ca1a63b4821
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-16 09:32:54 -0400

    Updating comments and printing statement in MayDailyProblems java file.

--- leetcode/MayDailyProblems.java
+++ leetcode/MayDailyProblems.java
@@ -35,6 +35,7 @@ public class MayDailyProblems {
             System.out.println("Day 13: Score After Flipping Matrix");
             System.out.println("Day 14: Path with Maximum Gold");
             System.out.println("Day 15: Find the Safest Path in a Grid");
+            System.out.println("Day 16: Evaluate Boolean Binary Tree");
             System.out.println("88: Exit");
             int day = scan.nextInt();
             switch(day){
@@ -219,6 +220,8 @@ public class MayDailyProblems {
                     }
                     System.out.println("The maximum safeness factor of the path is: " + maximumSafenessFactor(grid3));
                     break;
+                case 16:
+                
                 case 88:
                     System.out.println("Thank you for using the May Daily Leetcode Problems :)");
                     System.exit(0);
@@ -846,4 +849,36 @@ public class MayDailyProblems {
         int n = mat.length;
         return i >= 0 && j >= 0 && i < n && j < n;
     }
+
+
+    // Day 16: Evaluate Boolean Binary Tree - Q2331(Easy)
+    /*
+    You are given the root of a full binary tree with the following properties:
+    * Leaf nodes have either the value 0 or 1, where 0 represents False and 1 represents True.
+    * on-leaf nodes have either the value 2 or 3, where 2 represents the boolean OR and 3 represents the boolean AND.
+     
+    The evaluation of a node is as follows:
+    if the node is a leaf node, the evaluation is the value of the node, i.e. True or False.
+    Otherwise, evaluate the node's two children and apply the boolean operation of its value with the children's evaluations.
+        
+    Return the boolean result of evaluating the root node.
+
+    A full binary tree is a binary tree where each node has either 0 or 2 children.
+
+    A leaf node is a node that has zero children.
+
+    Example 1:
+    Input: root = [2,1,3,null,null,0,1]
+    Output: true
+    Explanation: The above diagram illustrates the evaluation process.
+    The AND node evaluates to False AND True = False.
+    The OR node evaluates to True OR False = True.
+    The root node evaluates to True, so we return true.
+
+
+    Example 2:
+    Input: root = [0]
+    Output: false
+    Explanation: The root node is a leaf node and it evaluates to false, so we return false.
+    */
 }
