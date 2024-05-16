0103aadf0e6e861d32889d5c6cdf0d08eb174c5a
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-16 08:21:30 -0400

    Added solution and comments to the cpp file for Day 16

--- leetcode/MayDailyProblems.cpp
+++ leetcode/MayDailyProblems.cpp
@@ -14,27 +14,27 @@ class Solutions{
         }
     };
 
-    class TreeNode{
-        public:
-        int val;
-        TreeNode* left;
-        TreeNode* right;
-        TreeNode(){
-            this->val = 0;
-            this->left = nullptr;
-            this->right = nullptr;
-        }
-        TreeNode(int val) {
-            this->val = val;
-            this->left = nullptr;
-            this->right = nullptr;
-        }
-        TreeNode(int val, TreeNode* left, TreeNode* right) {
-            this->val = val;
-            this->left = left;
-            this->right = right;
-        }
-    };
+    // class TreeNode{
+    //     public:
+    //     int val;
+    //     TreeNode* left;
+    //     TreeNode* right;
+    //     TreeNode(){
+    //         this->val = 0;
+    //         this->left = nullptr;
+    //         this->right = nullptr;
+    //     }
+    //     TreeNode(int val) {
+    //         this->val = val;
+    //         this->left = nullptr;
+    //         this->right = nullptr;
+    //     }
+    //     TreeNode(int val, TreeNode* left, TreeNode* right) {
+    //         this->val = val;
+    //         this->left = left;
+    //         this->right = right;
+    //     }
+    // };
     struct TreeNode {
         int val;
         TreeNode *left;
@@ -668,39 +668,53 @@ class Solutions{
             }
         }
         return false;
-
-
-        // Day 16: Evaluate Boolean Binary Tree - Q2331(Easy)
-        /*
-        You are given the root of a full binary tree with the following properties:
-         * Leaf nodes have either the value 0 or 1, where 0 represents False and 1 represents True.
-         * on-leaf nodes have either the value 2 or 3, where 2 represents the boolean OR and 3 represents the boolean AND.
-        
-        The evaluation of a node is as follows:
-        If the node is a leaf node, the evaluation is the value of the node, i.e. True or False.
-        Otherwise, evaluate the node's two children and apply the boolean operation of its value with the children's evaluations.
-        
-        Return the boolean result of evaluating the root node.
-
-        A full binary tree is a binary tree where each node has either 0 or 2 children.
-
-        A leaf node is a node that has zero children.
-
-        Example 1:
-        Input: root = [2,1,3,null,null,0,1]
-        Output: true
-        Explanation: The above diagram illustrates the evaluation process.
-        The AND node evaluates to False AND True = False.
-        The OR node evaluates to True OR False = True.
-        The root node evaluates to True, so we return true.
-
-
-        Example 2:
-        Input: root = [0]
-        Output: false
-        Explanation: The root node is a leaf node and it evaluates to false, so we return false.
-        */
     }
+
+
+    // Day 16: Evaluate Boolean Binary Tree - Q2331(Easy)
+    /*
+    You are given the root of a full binary tree with the following properties:
+    * Leaf nodes have either the value 0 or 1, where 0 represents False and 1 represents True.
+    * on-leaf nodes have either the value 2 or 3, where 2 represents the boolean OR and 3 represents the boolean AND.
+     
+    The evaluation of a node is as follows:
+    if the node is a leaf node, the evaluation is the value of the node, i.e. True or False.
+    Otherwise, evaluate the node's two children and apply the boolean operation of its value with the children's evaluations.
+        
+    Return the boolean result of evaluating the root node.
+
+    A full binary tree is a binary tree where each node has either 0 or 2 children.
+
+    A leaf node is a node that has zero children.
+
+    Example 1:
+    Input: root = [2,1,3,null,null,0,1]
+    Output: true
+    Explanation: The above diagram illustrates the evaluation process.
+    The AND node evaluates to False AND True = False.
+    The OR node evaluates to True OR False = True.
+    The root node evaluates to True, so we return true.
+
+
+    Example 2:
+    Input: root = [0]
+    Output: false
+    Explanation: The root node is a leaf node and it evaluates to false, so we return false.
+    */
+    bool evaluateTree(TreeNode* root){
+            if (root->left == nullptr and root->right == nullptr) return root->val;
+            bool evaluateLeftSubtree = evaluateTree(root->left);
+            bool evaluateRightSubtree = evaluateTree(root->right);
+            bool evaluateRoot;
+            if (root->val == 2) {
+                evaluateRoot = evaluateLeftSubtree | evaluateRightSubtree;
+            } else {
+                evaluateRoot = evaluateLeftSubtree & evaluateRightSubtree;
+            }
+
+            return evaluateRoot;
+    }
+    
 };
 
 int main() {
@@ -937,7 +951,31 @@ int main() {
                 break;
             }
             case 16:{
-
+                cout << "Enter the number of nodes in the binary tree:" << '\n';
+                int n; cin >> n;
+                cout << "Enter the values of the nodes in the binary tree:" << '\n';
+                vector<int> nodes(n);
+                for (int i = 0; i < n; i++) cin >> nodes[i];
+                Solutions::TreeNode* root = new Solutions::TreeNode(nodes[0]);
+                queue<Solutions::TreeNode*> q;
+                q.push(root);
+                int i = 1;
+                while (!q.empty() && i < n) {
+                    Solutions::TreeNode* node = q.front();
+                    q.pop();
+                    if (nodes[i] != -1) {
+                        node->left = new Solutions::TreeNode(nodes[i]);
+                        q.push(node->left);
+                    }
+                    i++;
+                    if (i < n && nodes[i] != -1) {
+                        node->right = new Solutions::TreeNode(nodes[i]);
+                        q.push(node->right);
+                    }
+                    i++;
+                }
+                cout << "The boolean result of evaluating the root node is: " << sol.evaluateTree(root) << '\n';
+                break;
             }
             case 88:
                 cout << "Thank you for using the May Daily Leetcode Problems :)\n";
