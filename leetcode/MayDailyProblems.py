74619fb5380381be67446ae158b244c261e6695d
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-16 09:40:14 -0400

    Updating comments and printing statement in MayDailyProblems python file.

--- leetcode/MayDailyProblems.py
+++ leetcode/MayDailyProblems.py
@@ -541,6 +541,37 @@ class Solutions:
                     traversal_queue.append((di, dj))
 
         return False
+    
+    # Day 16: Evaluate Boolean Binary Tree - Q2331(Easy)
+    '''
+    You are given the root of a full binary tree with the following properties:
+    * Leaf nodes have either the value 0 or 1, where 0 represents False and 1 represents True.
+    * on-leaf nodes have either the value 2 or 3, where 2 represents the boolean OR and 3 represents the boolean AND.
+     
+    The evaluation of a node is as follows:
+    if the node is a leaf node, the evaluation is the value of the node, i.e. True or False.
+    Otherwise, evaluate the node's two children and apply the boolean operation of its value with the children's evaluations.
+        
+    Return the boolean result of evaluating the root node.
+
+    A full binary tree is a binary tree where each node has either 0 or 2 children.
+
+    A leaf node is a node that has zero children.
+
+    Example 1:
+    Input: root = [2,1,3,null,null,0,1]
+    Output: true
+    Explanation: The above diagram illustrates the evaluation process.
+    The AND node evaluates to False AND True = False.
+    The OR node evaluates to True OR False = True.
+    The root node evaluates to True, so we return true.
+
+
+    Example 2:
+    Input: root = [0]
+    Output: false
+    Explanation: The root node is a leaf node and it evaluates to false, so we return false.
+    '''
 
 
 def main():
@@ -691,6 +722,8 @@ def main():
             for _ in range(n):
                 grid.append(list(map(int, input().split())))
             print("The safest path in the grid is:", sol.maximumSafenessFactor(grid))
+        elif day == 16:
+            
         else:
             print("Sorry, the problem for the day you entered is not available")
 
