280271f572028201170847158717375b2eb36bae
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-18 23:03:18 -0400

    Updating comments and printing statement in MayDailyProblems cpp file.

--- leetcode/MayDailyProblems.cpp
+++ leetcode/MayDailyProblems.cpp
@@ -741,6 +741,23 @@ class Solutions{
         }
         return root;
     }
+
+    // Day 18: Find the Maximum Sum of Node Values - Q3068(Hard)
+    /*
+    There exists an undirected tree with n nodes numbered 0 to n - 1. 
+    You are given a 0-indexed 2D integer array edges of length n - 1, where edges[i] = [ui, vi] indicates that there is an edge between nodes ui and vi in the tree. 
+    You are also given a positive integer k, and a 0-indexed array of non-negative integers nums of length n, where nums[i] represents the value of the node numbered i.
+
+    Alice wants the sum of values of tree nodes to be maximum, for which Alice can perform the following operation any number of times (including zero) on the tree:
+
+    Choose any edge [u, v] connecting the nodes u and v, and update their values as follows:
+    nums[u] = nums[u] XOR k
+    nums[v] = nums[v] XOR k
+    Return the maximum possible sum of the values Alice can achieve by performing the operation any number of times.
+    */
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+        
+    }
 };
 
 int main() {
@@ -1044,6 +1061,9 @@ int main() {
                     }
                 }
                 cout << '\n';
+                break;
+            }
+            case 18:{
                 break;
             }
             case 88:
