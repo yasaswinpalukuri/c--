726ddda2e6784e16035ef5352a05c6d5f56fea57
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-19 13:47:58 -0400

    Added solution and comments to the cpp file for Day 19

--- leetcode/MayDailyProblems.cpp
+++ leetcode/MayDailyProblems.cpp
@@ -781,30 +781,7 @@ class Solutions{
         return (current->val - 1) + leftCoins + rightCoins;
     }
 
-    
-//     long long maximumValueSum(vector<int>& nums, int k,vector<vector<int>>& edges) {
-//         vector<vector<long long>> memo(nums.size(), vector<long long>(2, -1));
-//         return maxSumOfNodes(0, 1, nums, k, memo);
-//     }
-
-//     long long maxSumOfNodes(int index, int isEven, vector<int>& nums, int k,
-//                             vector<vector<long long>>& memo) {
-//         if (index == nums.size()) {
-//             // If the operation is performed on an odd number of elements return
-//             // INT_MIN
-//             return isEven == 1 ? 0 : INT_MIN;
-//         }
-//         if (memo[index][isEven] != -1) {
-//             return memo[index][isEven];
-//         }
-//         long long noXorDone =
-//             nums[index] + maxSumOfNodes(index + 1, isEven, nums, k, memo);
-//         long long xorDone = (nums[index] ^ k) +
-//                             maxSumOfNodes(index + 1, isEven ^ 1, nums, k, memo);
-
-//         return memo[index][isEven] = max(xorDone, noXorDone);
-//     }
-// Day 19: Find the Maximum Sum of Node Values - Q3068(Hard)
+    // Day 19: Find the Maximum Sum of Node Values - Q3068(Hard)
     /*
     There exists an undirected tree with n nodes numbered 0 to n - 1. 
     You are given a 0-indexed 2D integer array edges of length n - 1, where edges[i] = [ui, vi] indicates that there is an edge between nodes ui and vi in the tree. 
@@ -817,6 +794,29 @@ class Solutions{
     nums[v] = nums[v] XOR k
     Return the maximum possible sum of the values Alice can achieve by performing the operation any number of times.
     */
+    long long maximumValueSum(vector<int>& nums, int k,vector<vector<int>>& edges) {
+        vector<vector<long long>> memo(nums.size(), vector<long long>(2, -1));
+        return maxSumOfNodes(0, 1, nums, k, memo);
+    }
+
+    long long maxSumOfNodes(int index, int isEven, vector<int>& nums, int k,
+                            vector<vector<long long>>& memo) {
+        if (index == nums.size()) {
+            // If the operation is performed on an odd number of elements return
+            // INT_MIN
+            return isEven == 1 ? 0 : INT_MIN;
+        }
+        if (memo[index][isEven] != -1) {
+            return memo[index][isEven];
+        }
+        long long noXorDone =
+            nums[index] + maxSumOfNodes(index + 1, isEven, nums, k, memo);
+        long long xorDone = (nums[index] ^ k) +
+                            maxSumOfNodes(index + 1, isEven ^ 1, nums, k, memo);
+
+        return memo[index][isEven] = max(xorDone, noXorDone);
+    }
+
 };
 
 int main() {
