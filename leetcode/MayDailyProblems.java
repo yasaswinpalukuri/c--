1d610c9208b9b7c9d20d826faf8d58afc016e0d8
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-21 09:07:32 -0400

    Added solution and comments to the java file for Day 21

--- leetcode/MayDailyProblems.java
+++ leetcode/MayDailyProblems.java
@@ -1162,16 +1162,16 @@ public class MayDailyProblems {
         Output: [[],[0]]
     */
     static public List<List<Integer>> subsets(int[] nums) {
-        // List<List<Integer>> res = new ArrayList<>();
-        // res.add(new ArrayList<>());
-        // for (int num : nums) {
-        //     int size = res.size();
-        //     for (int i = 0; i < size; i++) {
-        //         List<Integer> subset = new ArrayList<>(res.get(i));
-        //         subset.add(num);
-        //         res.add(subset);
-        //     }
-        // }
-        // return res;
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        for (int num : nums) {
+            int size = res.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(res.get(i));
+                subset.add(num);
+                res.add(subset);
+            }
+        }
+        return res;
     }
 }
