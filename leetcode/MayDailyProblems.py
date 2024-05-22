77914ccb2640f051ab79c03b0062562ee8f83815
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-22 07:13:18 -0400

    Added solution and comments to the python file for Day 22

--- leetcode/MayDailyProblems.py
+++ leetcode/MayDailyProblems.py
@@ -749,6 +749,21 @@ class Solutions:
         Output: [["a"]]
     '''
     def partition(self, s: str) -> list[list[str]]:
+        def is_palindrome(sub):
+            return sub == sub[::-1]
+
+        def backtrack(start, path):
+            if start == len(s):
+                result.append(path[:])
+                return
+            for end in range(start + 1, len(s) + 1):
+                if is_palindrome(s[start:end]):
+                    backtrack(end, path + [s[start:end]])
+
+        result = []
+        backtrack(0, [])
+        return result
+
 
 
 
