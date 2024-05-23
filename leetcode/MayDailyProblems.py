85b2b9aa1040fbb6b521785c3b68429d31012044
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-23 09:00:02 -0400

    Added solution and comments to the python file for Day 23

--- leetcode/MayDailyProblems.py
+++ leetcode/MayDailyProblems.py
@@ -1,5 +1,6 @@
 import heapq
 from collections import deque
+from collections import defaultdict
 
 
 class Solutions:
@@ -786,9 +787,33 @@ class Solutions:
         Explanation: The beautiful subset of the array nums is [1].
         It can be proved that there is only 1 beautiful subset in the array [1].
     '''
-    def countBeautifulSubsets(self, nums: list[int], k: int) -> int:
+    def beautifulSubsets(self, nums: list[int], k: int) -> int:
+        total_count = 1
 
+        freq_map = defaultdict(dict)
+        for num in nums:
+            remainder = num % k
+            freq_map[remainder][num] = freq_map[remainder].get(num, 0) + 1
 
+        for fr in freq_map.values():
+            n = len(fr)
+
+            subsets = sorted(fr.items())
+            counts = [0] * (n + 1)
+            counts[n] = 1
+
+            for i in range(n - 1, -1, -1):
+                skip = counts[i + 1]
+                take = 2 ** subsets[i][1] - 1
+                if i + 1 < n and subsets[i + 1][0] - subsets[i][0] == k:
+                    take *= counts[i + 2]
+                else:
+                    take *= counts[i + 1]
+                counts[i] = skip + take
+
+            total_count *= counts[0]
+
+        return total_count - 1
 
 
 
@@ -998,7 +1023,7 @@ def main():
         elif day == 23:
             nums = list(map(int, input("Enter the elements of the array: ").split()))
             k = int(input("Enter the value of k: "))
-            print("The number of non-empty beautiful subsets of the array nums is:", sol.countBeautifulSubsets(nums, k))
+            print("The number of non-empty beautiful subsets of the array nums is:", sol.beautifulSubsets(nums, k))
         else:
             print("Sorry, the problem for the day you entered is not available")
 
