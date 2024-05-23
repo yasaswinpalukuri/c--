99773b72a6f6825edaf560950c6ceb7eadd367e3
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-23 08:52:04 -0400

    Added solution and comments to the cpp file for Day 23

--- leetcode/MayDailyProblems.cpp
+++ leetcode/MayDailyProblems.cpp
@@ -937,33 +937,33 @@ class Solutions{
         It can be proved that there is only 1 beautiful subset in the array [1].
     */
     int beautifulSubsets(vector<int>& nums, int k) {
-        // int totalCount = 1;
-        // map<int, map<int, int>> freqMap;
-        // for (int& num : nums) {
-        //     freqMap[num % k][num]++;
-        // }
-        // for (auto& fr : freqMap) {
-        //     vector<pair<int, int>> subsets(fr.second.begin(), fr.second.end());
-        //     totalCount *= countBeautifulSubsets(subsets, subsets.size(), k, 0);
-        // }
+        int totalCount = 1;
+        map<int, map<int, int>> freqMap;
+        for (int& num : nums) {
+            freqMap[num % k][num]++;
+        }
+        for (auto& fr : freqMap) {
+            vector<pair<int, int>> subsets(fr.second.begin(), fr.second.end());
+            totalCount *= countBeautifulSubsets(subsets, subsets.size(), k, 0);
+        }
 
-        // return totalCount - 1;
+        return totalCount - 1;
     }
 
     int countBeautifulSubsets(vector<pair<int, int>>& subsets, int numSubsets,int difference, int i){
-        // if (i == numSubsets) {
-        //     return 1;
-        // }
-        // int skip = countBeautifulSubsets(subsets, numSubsets, difference, i + 1);
-        // int take = (1 << subsets[i].second) - 1;
-        // if (i + 1 < numSubsets &&
-        //     subsets[i + 1].first - subsets[i].first == difference) {
-        //     take *= countBeautifulSubsets(subsets, numSubsets, difference, i + 2);
-        // } else {
-        //     take *= countBeautifulSubsets(subsets, numSubsets, difference, i + 1);
-        // }
+        if (i == numSubsets) {
+            return 1;
+        }
+        int skip = countBeautifulSubsets(subsets, numSubsets, difference, i + 1);
+        int take = (1 << subsets[i].second) - 1;
+        if (i + 1 < numSubsets &&
+            subsets[i + 1].first - subsets[i].first == difference) {
+            take *= countBeautifulSubsets(subsets, numSubsets, difference, i + 2);
+        } else {
+            take *= countBeautifulSubsets(subsets, numSubsets, difference, i + 1);
+        }
 
-        // return skip + take;
+        return skip + take;
     }
 };
 
