c838c0ff26801579c60e084a25e8e623ac18b28c
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-24 10:56:13 -0400

    Added solution and comments to the python file for Day 24

--- leetcode/MayDailyProblems.py
+++ leetcode/MayDailyProblems.py
@@ -849,43 +849,43 @@ class Solutions:
         Letter "e" can only be used once.
     '''
     def maxScoreWords(self, words: list[str], letters: list[str], score: list[int]) -> int:
-        # W = len(words)
-        # self.max_score = 0
-        # freq = [0 for i in range(26)]
-        # subset_letters = [0 for i in range(26)]
-        # for c in letters:
-        #     freq[ord(c) - 97] += 1
+        W = len(words)
+        self.max_score = 0
+        freq = [0 for i in range(26)]
+        subset_letters = [0 for i in range(26)]
+        for c in letters:
+            freq[ord(c) - 97] += 1
 
-        # def is_valid_word(subset_letters):
-        #     for c in range(26):
-        #         if freq[c] < subset_letters[c]:
-        #             return False
-        #     else:
-        #         return True
+        def is_valid_word(subset_letters):
+            for c in range(26):
+                if freq[c] < subset_letters[c]:
+                    return False
+            else:
+                return True
         
-        # def check(w, words, score, subset_letters, total_score):
-        #     if w == -1:
-        #         self.max_score = max(self.max_score, total_score)
-        #         return
+        def check(w, words, score, subset_letters, total_score):
+            if w == -1:
+                self.max_score = max(self.max_score, total_score)
+                return
             
-        #     check(w - 1, words, score, subset_letters, total_score)
+            check(w - 1, words, score, subset_letters, total_score)
 
-        #     L = len(words[w])
-        #     for i in range(L):
-        #         c = ord(words[w][i]) - 97
-        #         subset_letters[c] += 1
-        #         total_score += score[c]
+            L = len(words[w])
+            for i in range(L):
+                c = ord(words[w][i]) - 97
+                subset_letters[c] += 1
+                total_score += score[c]
 
-        #     if is_valid_word(subset_letters):
-        #         check(w - 1, words, score, subset_letters, total_score)
+            if is_valid_word(subset_letters):
+                check(w - 1, words, score, subset_letters, total_score)
                 
-        #     for i in range(L):
-        #         c = ord(words[w][i]) - 97
-        #         subset_letters[c] -= 1
-        #         total_score -= score[c]
+            for i in range(L):
+                c = ord(words[w][i]) - 97
+                subset_letters[c] -= 1
+                total_score -= score[c]
 
-        # check(W - 1, words, score, subset_letters, 0)
-        # return self.max_score
+        check(W - 1, words, score, subset_letters, 0)
+        return self.max_score
 
 
 
