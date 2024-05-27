cc9f5d0240406fb57f9e8bebcc9bc9ea6a88f5b8
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-27 08:12:16 -0400

    Updating comments and printing statement in MayDailyProblems cpp file.

--- leetcode/MayDailyProblems.cpp
+++ leetcode/MayDailyProblems.cpp
@@ -1184,6 +1184,55 @@ class Solutions{
         c = (c + checkCombinations(n-1,a,l+1))%1000000007; // "L" case
         return comb[n][a][l] = c;
     }
+
+
+    // Day 27: Special Array With X Elements Greater Than or Equal X - Q1608(Easy)
+    /*
+        You are given an array nums of non-negative integers. nums is considered special if there exists a number x such that there are exactly x numbers in nums that are greater than or equal to x.
+
+        Notice that x does not have to be an element in nums.
+
+        Return x if the array is special, otherwise, return -1. It can be proven that if nums is special, the value for x is unique.
+
+        Example 1:
+        Input: nums = [3,5]
+        Output: 2
+        Explanation: There are 2 values (3 and 5) that are greater than or equal to 2.
+        
+        Example 2:
+        Input: nums = [0,0]
+        Output: -1
+        Explanation: No numbers fit the criteria for x.
+        If x = 0, there should be 0 numbers >= x, but there are 2.
+        If x = 1, there should be 1 number >= x, but there are 0.
+        If x = 2, there should be 2 numbers >= x, but there are 0.
+        x cannot be greater since there are only 2 numbers in nums.
+        
+        Example 3:
+        Input: nums = [0,4,3,0,4]
+        Output: 3
+        Explanation: There are 3 values that are greater than or equal to 3.
+        
+
+        Constraints:
+        1 <= nums.length <= 100
+        0 <= nums[i] <= 1000
+    */
+    int specialArray(vector<int>& nums) {
+        // int n = nums.size();
+        // for (int x = 0; x <= n; x++) {
+        //     int count = 0;
+        //     for (int num : nums) {
+        //         if (num >= x) {
+        //             count++;
+        //         }
+        //     }
+        //     if (count == x) {
+        //         return x;
+        //     }
+        // }
+        // return -1;
+    }
 };
 
 int main() {
@@ -1219,6 +1268,7 @@ int main() {
         cout << "Day 24: Maximum Score Words Formed by Letters\n";
         cout << "Day 25: Word Break II\n";
         cout << "Day 26: Student Attendance Record II\n";
+        cout << "Day 27: Special Array With X Elements Greater Than or Equal X\n";
         cout << "88: Exit" << '\n';
         int day; cin >> day;
         Solutions sol;
@@ -1623,6 +1673,15 @@ int main() {
                 cout << "Enter the number of days:" << '\n';
                 int n; cin >> n;
                 cout << "The number of possible attendance records of length n that make a student eligible for an attendance award is: " << sol.checkRecords(n) << '\n';
+                break;
+            }
+            case 27:{
+                cout << "Enter the number of elements in the array:" << '\n';
+                int n; cin >> n;
+                vector<int> nums(n);
+                cout << "Enter the elements of the array:" << '\n';
+                for (int i = 0; i < n; i++) cin >> nums[i];
+                cout << "The value of x if the array is special is: " << sol.specialArray(nums) << '\n';
                 break;
             }
             case 88:
