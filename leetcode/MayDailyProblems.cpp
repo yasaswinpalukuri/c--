5f01599ab45193665a77a321e845f228ecb9a394
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-27 08:14:22 -0400

    Added solution and comments to the cpp file for Day 27

--- leetcode/MayDailyProblems.cpp
+++ leetcode/MayDailyProblems.cpp
@@ -1219,19 +1219,19 @@ class Solutions{
         0 <= nums[i] <= 1000
     */
     int specialArray(vector<int>& nums) {
-        // int n = nums.size();
-        // for (int x = 0; x <= n; x++) {
-        //     int count = 0;
-        //     for (int num : nums) {
-        //         if (num >= x) {
-        //             count++;
-        //         }
-        //     }
-        //     if (count == x) {
-        //         return x;
-        //     }
-        // }
-        // return -1;
+        int n = nums.size();
+        for (int x = 0; x <= n; x++) {
+            int count = 0;
+            for (int num : nums) {
+                if (num >= x) {
+                    count++;
+                }
+            }
+            if (count == x) {
+                return x;
+            }
+        }
+        return -1;
     }
 };
 
