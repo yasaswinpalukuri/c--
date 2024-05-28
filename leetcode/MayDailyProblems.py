e311c75a0400420c6e6c49709d3c4ec5ff56b711
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-28 08:04:11 -0400

    Added solution and comments to the python file for Day 28

--- leetcode/MayDailyProblems.py
+++ leetcode/MayDailyProblems.py
@@ -1093,6 +1093,13 @@ class Solutions:
 
     '''
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        left = 0
+        for right in range(len(s)):
+            maxCost -= abs(ord(s[right]) - ord(t[right]))
+            if maxCost < 0:
+                maxCost += abs(ord(s[left]) - ord(t[left]))
+                left += 1
+        return right - left + 1
 
 
 
