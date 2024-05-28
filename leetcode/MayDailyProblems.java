049404a5ca9017d244cbc953dbd15ce40b4e9770
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-28 07:57:10 -0400

    Updating comments and printing statement in MayDailyProblems java file.

--- leetcode/MayDailyProblems.java
+++ leetcode/MayDailyProblems.java
@@ -79,6 +79,7 @@ public class MayDailyProblems {
             System.out.println("Day 25: Word Break II");
             System.out.println("Day 26: Student Attendance Record II");
             System.out.println("Day 27: Special Array With X Elements Greater Than or Equal X");
+            System.out.println("Day 28: Get Equal Substrings Within Budget");
             System.out.println("88: Exit");
             int day = scan.nextInt();
             switch(day){
@@ -394,6 +395,15 @@ public class MayDailyProblems {
                         nums27[i] = scan.nextInt();
                     }
                     System.out.println("The number of special arrays is: " + specialArray(nums27));
+                    break;
+                case 28:
+                    System.out.println("Enter the string:");
+                    String s28 = scan.next();
+                    System.out.println("Enter the string:");
+                    String t28 = scan.next();
+                    System.out.println("Enter the maximum cost:");
+                    int maxCost = scan.nextInt();
+                    System.out.println("The equal substrings within budget are: " + equalSubstring(s28, t28, maxCost);
                     break;
                 case 88:
                     System.out.println("Thank you for using the May Daily Leetcode Problems :)");
@@ -1615,5 +1625,62 @@ public class MayDailyProblems {
             }
         }
         return -1;
+    }
+
+
+    // Day 29: Get Equal Substrings Within Budget - Q1208(Medium)
+    /*
+        You are given two strings s and t of the same length and an integer maxCost.
+
+        You want to change s to t. Changing the ith character of s to ith character of t costs |s[i] - t[i]|
+        (i.e., the absolute difference between the ASCII values of the characters).
+
+        Return the maximum length of a substring of s that can be changed to be the same as the corresponding substring of t,
+        with a cost less than or equal to maxCost. If there is no substring from s that can be changed to its corresponding substring from t, return 0.
+
+        Example 1:
+        Input: s = "abcd", t = "bcdf", maxCost = 3
+        Output: 3
+        Explanation: "abc" of s can change to "bcd".
+        That costs 3, so the maximum length is 3.
+        
+        Example 2:
+        Input: s = "abcd", t = "cdef", maxCost = 3
+        Output: 1
+        Explanation: Each character in s costs 2 to change to character in t,  so the maximum length is 1.
+        
+        Example 3:
+        Input: s = "abcd", t = "acde", maxCost = 0
+        Output: 1
+        Explanation: You cannot make any change, so the maximum length is 1.
+        
+
+        Constraints:
+        1 <= s.length <= 105
+        t.length == s.length
+        0 <= maxCost <= 106
+        s and t consist of only lowercase English letters.
+
+    */
+    static public int equalSubstring(String s, String t, int maxCost) {
+        // int n = s.length();
+        // int[] cost = new int[n];
+        // for (int i = 0; i < n; i++) {
+        //     cost[i] = Math.abs(s.charAt(i) - t.charAt(i));
+        // }
+        // int maxLength = 0;
+        // int start = 0;
+        // int end = 0;
+        // int currentCost = 0;
+        // while (end < n) {
+        //     currentCost += cost[end];
+        //     while (currentCost > maxCost) {
+        //         currentCost -= cost[start];
+        //         start++;
+        //     }
+        //     maxLength = Math.max(maxLength, end - start + 1);
+        //     end++;
+        // }
+        // return maxLength;
     }
 }
