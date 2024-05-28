c03338523703b796d72c586f235e22990e4883db
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-28 07:55:00 -0400

    Added solution and comments to the cpp file for Day 28

--- leetcode/MayDailyProblems.cpp
+++ leetcode/MayDailyProblems.cpp
@@ -1269,6 +1269,27 @@ class Solutions{
         s and t consist of only lowercase English letters.
 
     */
+    int equalSubstring(string s, string t, int maxCost) {
+        int n = s.size();
+        vector<int> cost(n);
+        for (int i = 0; i < n; i++) {
+            cost[i] = abs(s[i] - t[i]);
+        }
+        int maxLength = 0;
+        int start = 0;
+        int end = 0;
+        int currentCost = 0;
+        while (end < n) {
+            currentCost += cost[end];
+            while (currentCost > maxCost) {
+                currentCost -= cost[start];
+                start++;
+            }
+            maxLength = max(maxLength, end - start + 1);
+            end++;
+        }
+        return maxLength;
+    }
 };
 
 int main() {
