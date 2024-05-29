bf7eac8488026f89f3633e6b527f1ef6daaf3c5b
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-29 06:15:03 -0400

    Added solution and comments to the cpp file for Day 29

--- leetcode/MayDailyProblems.cpp
+++ leetcode/MayDailyProblems.cpp
@@ -1330,6 +1330,16 @@ class Solutions{
         s[0] == '1'
     */
     int numSteps(string s) {
+        int n = s.length();
+        int ans = n - 1, carry = 0;
+
+        for (int i = n - 1; i > 0; i--) {
+            if (s[i] - '0' + carry == 1) {
+                ans++;
+                carry = 1;
+            }
+        }
+        return ans + carry;
     }
 };
 
