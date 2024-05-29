850ed316ee7cf6c99a074216918f7546169dd4ae
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-29 06:20:56 -0400

    Added solution and comments to the java file for Day 29

--- leetcode/MayDailyProblems.java
+++ leetcode/MayDailyProblems.java
@@ -1728,19 +1728,16 @@ public class MayDailyProblems {
         s[0] == '1'
     */
     static public int numSteps(String s) {
-        // int steps = 0;
-        // int carry = 0;
-        // for (int i = s.length() - 1; i > 0; i--) {
-        //     if (s.charAt(i) - '0' + carry == 1) {
-        //         steps += 2;
-        //         carry = 1;
-        //     } else {
-        //         steps++;
-        //         if (carry == 1) {
-        //             steps++;
-        //         }
-        //     }
-        // }
-        // return steps + carry;
+        int n = s.length();
+        int ans = n - 1;
+        int carry = 0;
+
+        for (int i = n - 1; i > 0; i--) {
+            if ((s.charAt(i) - '0') + carry == 1) {
+                ans++;
+                carry = 1;
+            }
+        }
+        return ans + carry;
     }
 }
