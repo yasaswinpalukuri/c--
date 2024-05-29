153081dbf919812cb8b9372bc97d7d193e71769f
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-29 06:07:47 -0400

    Updating comments and printing statement in MayDailyProblems cpp file.

--- leetcode/MayDailyProblems.cpp
+++ leetcode/MayDailyProblems.cpp
@@ -1290,6 +1290,47 @@ class Solutions{
         }
         return maxLength;
     }
+
+
+
+    // Day 29: Number of Steps to Reduce a Number in Binary Representation to One - Q1404(Medium)
+    /*
+        Given the binary representation of an integer as a string s, return the number of steps to reduce it to 1 under the following rules:
+
+        If the current number is even, you have to divide it by 2.
+
+        If the current number is odd, you have to add 1 to it.
+
+        It is guaranteed that you can always reach one for all test cases.
+
+        Example 1:
+        Input: s = "1101"
+        Output: 6
+        Explanation: "1101" corressponds to number 13 in their decimal representation.
+        Step 1) 13 is odd, add 1 and obtain 14. 
+        Step 2) 14 is even, divide by 2 and obtain 7.
+        Step 3) 7 is odd, add 1 and obtain 8.
+        Step 4) 8 is even, divide by 2 and obtain 4.  
+        Step 5) 4 is even, divide by 2 and obtain 2. 
+        Step 6) 2 is even, divide by 2 and obtain 1.  
+        
+        Example 2:
+        Input: s = "10"
+        Output: 1
+        Explanation: "10" corressponds to number 2 in their decimal representation.
+        Step 1) 2 is even, divide by 2 and obtain 1.  
+        
+        Example 3:
+        Input: s = "1"
+        Output: 0
+        
+        Constraints:
+        1 <= s.length <= 500
+        s consists of characters '0' or '1'
+        s[0] == '1'
+    */
+    int numSteps(string s) {
+    }
 };
 
 int main() {
@@ -1327,6 +1368,7 @@ int main() {
         cout << "Day 26: Student Attendance Record II\n";
         cout << "Day 27: Special Array With X Elements Greater Than or Equal X\n";
         cout << "Day 28: Get Equal Substrings Within Budget\n";
+        cout << "Day 29: Number of Steps to Reduce a Number in Binary Representation to One\n";
         cout << "88: Exit" << '\n';
         int day; cin >> day;
         Solutions sol;
@@ -1747,6 +1789,12 @@ int main() {
                 string s, t; int maxCost;
                 cin >> s >> t >> maxCost;
                 cout << "The maximum length of a substring of s that can be changed to be the same as the corresponding substring of t is: " << sol.equalSubstring(s, t, maxCost) << '\n';
+                break;
+            }
+            case 29:{
+                cout << "Enter the binary representation of the integer:" << '\n';
+                string s; cin >> s;
+                cout << "The number of steps to reduce the binary representation of the integer to 1 is: " << sol.numSteps(s) << '\n';
                 break;
             }
             case 88:
