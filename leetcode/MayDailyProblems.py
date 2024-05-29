6f41c9194fa705b0bbdfe84ed954cec9c055a896
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-29 06:22:22 -0400

    Updating comments and printing statement in MayDailyProblems python file.

--- leetcode/MayDailyProblems.py
+++ leetcode/MayDailyProblems.py
@@ -1101,6 +1101,54 @@ class Solutions:
                 left += 1
         return right - left + 1
 
+    # Day 29: Number of Steps to Reduce a Number in Binary Representation to One - Q1404(Medium)
+    '''
+        Given the binary representation of an integer as a string s, return the number of steps to reduce it to 1 under the following rules:
+
+        If the current number is even, you have to divide it by 2.
+
+        If the current number is odd, you have to add 1 to it.
+
+        It is guaranteed that you can always reach one for all test cases.
+
+        Example 1:
+        Input: s = "1101"
+        Output: 6
+        Explanation: "1101" corressponds to number 13 in their decimal representation.
+        Step 1) 13 is odd, add 1 and obtain 14. 
+        Step 2) 14 is even, divide by 2 and obtain 7.
+        Step 3) 7 is odd, add 1 and obtain 8.
+        Step 4) 8 is even, divide by 2 and obtain 4.  
+        Step 5) 4 is even, divide by 2 and obtain 2. 
+        Step 6) 2 is even, divide by 2 and obtain 1.  
+        
+        Example 2:
+        Input: s = "10"
+        Output: 1
+        Explanation: "10" corressponds to number 2 in their decimal representation.
+        Step 1) 2 is even, divide by 2 and obtain 1.  
+        
+        Example 3:
+        Input: s = "1"
+        Output: 0
+        
+        Constraints:
+        1 <= s.length <= 500
+        s consists of characters '0' or '1'
+        s[0] == '1'
+    '''
+    def numSteps(self, s: str) -> int:
+        # n = len(s)
+        # ans = n - 1
+        # carry = 0
+
+        # for i in range(n - 1, 0, -1):
+        #     if int(s[i]) + carry == 1:
+        #         ans += 1
+        #         carry = 1
+
+        # return ans + carry
+
 
 
 def main():
@@ -1140,6 +1188,7 @@ def main():
         print("Day 26: Student Attendance Record II")
         print("Day 27: Special Array With X Elements Greater Than or Equal X")
         print("Day 28: Get Equal Substrings Within Budget")
+        print("Day 29: Number of Steps to Reduce a Number in Binary Representation to One")
         print("88: Exit")
         
         day = int(input())
@@ -1362,6 +1411,10 @@ def main():
             t = input("Enter the second string: ")
             maxCost = int(input("Enter the maximum cost: "))
             print("The maximum length of a substring of s that can be changed to be the same as the corresponding substring of t is:", sol.equalSubstring(s, t, maxCost))
+        
+        elif day == 29:
+            s = input("Enter the binary representation of the number: ")
+            print("The number of steps to reduce the number to 1 is:", sol.numSteps(s))
         else:
             print("Sorry, the problem for the day you entered is not available")
 
