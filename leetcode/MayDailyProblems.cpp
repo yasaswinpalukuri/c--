b06a247e0bc013f77772faf05923a6e1c67d9798
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-30 05:55:19 -0400

    Added solution and comments to the cpp file for Day 30

--- leetcode/MayDailyProblems.cpp
+++ leetcode/MayDailyProblems.cpp
@@ -1368,7 +1368,16 @@ class Solutions{
         1 <= arr[i] <= pow(10,8)
     */
     int countTriplets(vector<int>& arr){
-
+        int size = arr.size();
+        int count = 0;
+        int prefix = 0;
+        unordered_map<int, int> countMap = {{0, 1}}, totalMap;
+        for (int i = 0; i < size; ++i) {
+            prefix ^= arr[i];
+            count += countMap[prefix]++ * i - totalMap[prefix];
+            totalMap[prefix] += i + 1;
+        }
+        return count;
     }
 };
 
