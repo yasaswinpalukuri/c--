520ad09dae2d22408e135fb5c91d39cf423b2f53
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-05-31 06:11:53 -0400

    Updating comments and printing statement in MayDailyProblems cpp file.

--- leetcode/MayDailyProblems.cpp
+++ leetcode/MayDailyProblems.cpp
@@ -1379,6 +1379,36 @@ class Solutions{
         }
         return count;
     }
+
+
+    // Day 31: Single Number III - Q260(Medium)
+    /*
+        Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. 
+        Find the two elements that appear only once. You can return the answer in any order.
+        You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+    
+
+        Example 1:
+        Input: nums = [1,2,1,3,2,5]
+        Output: [3,5]
+        Explanation:  [5, 3] is also a valid answer.
+        
+        Example 2:
+        Input: nums = [-1,0]
+        Output: [-1,0]
+        
+        Example 3:
+        Input: nums = [0,1]
+        Output: [1,0]
+
+        Constraints:
+        2 <= nums.length <= 3 * 10^4
+        -2^31 <= nums[i] <= 2^31 - 1
+        Each integer in nums will appear twice, only two integers will appear once.
+    */
+    vector<int> singleNumber(vector<int>& nums) {
+        
+    }
 };
 
 int main() {
@@ -1418,6 +1448,7 @@ int main() {
         cout << "Day 28: Get Equal Substrings Within Budget\n";
         cout << "Day 29: Number of Steps to Reduce a Number in Binary Representation to One\n";
         cout << "Day 30: Count Triplets That Can Form Two Arrays of Equal XOR\n";
+        cout << "Day 31: Single Number III\n";
         cout << "88: Exit" << '\n';
         int day; cin >> day;
         Solutions sol;
@@ -1853,6 +1884,16 @@ int main() {
                 cout << "Enter the elements of the array:" << '\n';
                 for (int i = 0; i < n; i++) cin >> arr[i];
                 cout << "The number of triplets that can form two arrays of equal XOR is: " << sol.countTriplets(arr) << '\n';
+                break;
+            }
+            case 31:{
+                cout << "Enter the number of elements in the array:" << '\n';
+                int n; cin >> n;
+                vector<int> nums(n);
+                cout << "Enter the elements of the array:" << '\n';
+                for (int i = 0; i < n; i++) cin >> nums[i];
+                vector<int> singleNums = sol.singleNumber(nums);
+                cout << "The two elements that appear only once in the array are: [" << singleNums[0] << "," << singleNums[1] << ']\n';
                 break;
             }
             case 88:
