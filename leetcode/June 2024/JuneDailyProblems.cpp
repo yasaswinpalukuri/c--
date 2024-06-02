9040bb10284231ea9abea0f8ca33a06d2eaa98c2
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-06-02 09:33:42 -0400

    cpp file update

--- leetcode/June 2024/JuneDailyProblems.cpp
+++ leetcode/June 2024/JuneDailyProblems.cpp
@@ -52,7 +52,7 @@ class Solution{
         s[i] is a printable ascii character.
     
     */
-   void reverseString(vector<char>& s) {
+    void reverseString(vector<char>& s) {
         int n = s.size();
         for(int i=0;i<n/2;i++) swap(s[i],s[n-i-1]);
     }
