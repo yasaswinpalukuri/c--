b6f8d952f3334c766189a35c62dd64b79360c181
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-06-03 06:52:42 -0400

    Added solution and comments to the java file for Day 3

--- leetcode/June 2024/JuneDailyProblems.java
+++ leetcode/June 2024/JuneDailyProblems.java
@@ -151,14 +151,14 @@ class Solution {
         s and t consist only of lowercase English letters.
     */
     public int appendCharacters(String s, String t) {
-        // int i = 0, j = 0;
-        // while(i < s.length() && j < t.length()) {
-        //     if(s.charAt(i) == t.charAt(j)) {
-        //         j++;
-        //     }
-        //     i++;
-        // }
-        // return t.length() - j;
+        int i = 0, j = 0;
+        while(i < s.length() && j < t.length()) {
+            if(s.charAt(i) == t.charAt(j)) {
+                j++;
+            }
+            i++;
+        }
+        return t.length() - j;
     }
 }
 
