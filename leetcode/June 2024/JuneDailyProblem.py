6d77036231425216cbc0dd04c233b8fec6521c88
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-06-03 06:54:08 -0400

    Updating comments and printing statement in MayDailyProblems python file.

--- leetcode/June 2024/JuneDailyProblem.py
+++ leetcode/June 2024/JuneDailyProblem.py
@@ -54,6 +54,40 @@ class Solutions:
             s[l], s[r] = s[r], s[l]
             l += 1
             r -= 1
+    
+    # Day 3: Append Characters to String to Make Subsequence - Q2468(Medium)
+    '''
+        You are given two strings s and t consisting of only lowercase English letters.
+
+        Return the minimum number of characters that need to be appended to the end of s so that t becomes a subsequence of s.
+        A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+
+        Example 1:
+        Input: s = "coaching", t = "coding"
+        Output: 4
+        Explanation: Append the characters "ding" to the end of s so that s = "coachingding".
+        Now, t is a subsequence of s ("coachingding").
+        It can be shown that appending any 3 characters to the end of s will never make t a subsequence.
+        
+        Example 2:
+        Input: s = "abcde", t = "a"
+        Output: 0
+        Explanation: t is already a subsequence of s ("abcde").
+        
+        Example 3:
+        Input: s = "z", t = "abcde"
+        Output: 5
+        Explanation: Append the characters "abcde" to the end of s so that s = "zabcde".
+        Now, t is a subsequence of s ("zabcde").
+        It can be shown that appending any 4 characters to the end of s will never make t a subsequence.
+        
+
+        Constraints:
+        1 <= s.length, t.length <= 10^5
+        s and t consist only of lowercase English letters.
+    '''
+    def appendChracters(self, s: str, t: str) -> int:
+        
 
 class JuneDailyProblems(Solutions):
     def __init__(self):
@@ -68,6 +102,7 @@ class JuneDailyProblems(Solutions):
             print("Enter the day of the problem you want the answer for or 88 to Exit:")
             print("Day 1: Score of a String")
             print("Day 2: Reverse String")
+            print("Day 3: Append Characters to String to Make Subsequence")
             print("88. Exit")
             day = int(input("Enter your choice: "))
             if day == 1:
@@ -79,6 +114,11 @@ class JuneDailyProblems(Solutions):
                 self.reverseString(word)
                 print("The reversed string is: ", word)
 
+            elif day == 3:
+                s = input("Enter the first string: ")
+                t = input("Enter the second string: ")
+                print("The minimum number of characters that need to be appended to the end of s so that t becomes a subsequence of s is: ", self.appendChracters(s, t))
+                
             elif day == 88:
                 print("Thank you for using the June Daily Leetcode Problems:)")
                 break
