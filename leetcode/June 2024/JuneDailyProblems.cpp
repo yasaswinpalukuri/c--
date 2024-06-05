b0b753434ed419891d7428066c0b45c65b5925fc
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-06-05 07:55:34 -0400

    Added solution and comments to the cpp file for Day 5

--- leetcode/June 2024/JuneDailyProblems.cpp
+++ leetcode/June 2024/JuneDailyProblems.cpp
@@ -157,17 +157,17 @@ class Solution{
         words[i] consists of lowercase English letters.
     */
     vector<string> commonChars(vector<string>& words) {
-        // vector<int> cnt(26,INT_MAX);
-        // for(auto word:words){
-        //     vector<int> cnt1(26,0);
-        //     for(auto c:word) cnt1[c-'a']++;
-        //     for(int i=0;i<26;i++) cnt[i] = min(cnt[i],cnt1[i]);
-        // }
-        // vector<string> ans;
-        // for(int i=0;i<26;i++){
-        //     for(int j=0;j<cnt[i];j++) ans.push_back(string(1,'a'+i));
-        // }
-        // return ans;
+        vector<int> cnt(26,INT_MAX);
+        for(auto word:words){
+            vector<int> cnt1(26,0);
+            for(auto c:word) cnt1[c-'a']++;
+            for(int i=0;i<26;i++) cnt[i] = min(cnt[i],cnt1[i]);
+        }
+        vector<string> ans;
+        for(int i=0;i<26;i++){
+            for(int j=0;j<cnt[i];j++) ans.push_back(string(1,'a'+i));
+        }
+        return ans;
     }
 };
 
