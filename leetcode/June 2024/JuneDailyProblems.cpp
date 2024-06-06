841314ed2cdfc93aad4b1415fbf8e52ec87bc2ec
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-06-06 06:53:57 -0400

    Updating comments and printing statement in JuneDailyProblems cpp file.

--- leetcode/June 2024/JuneDailyProblems.cpp
+++ leetcode/June 2024/JuneDailyProblems.cpp
@@ -169,6 +169,43 @@ class Solution{
         }
         return ans;
     }
+
+
+    // Day 6: Hand of Straights - Q846(Medium)
+    /*
+        Alice has a hand of cards, given as an array of integers.
+
+        Now she wants to rearrange the cards into groups so that each group is size W, and consists of W consecutive cards.
+
+        Return true if and only if she can.
+
+        Example 1:
+        Input: hand = [1,2,3,6,2,3,4,7,8], W = 3
+        Output: true
+        Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8].
+
+        Example 2:
+        Input: hand = [1,2,3,4,5], W = 4
+        Output: false
+        Explanation: Alice's hand can't be rearranged into groups of 4.
+
+        Constraints:
+        1 <= hand.length <= 10000
+        0 <= hand[i] <= 10^9
+        1 <= W <= hand.length
+    */
+    bool isNStraightHand(vector<int>& hand, int W) {
+        // map<int,int> mp;
+        // for(auto h:hand) mp[h]++;
+        // for(auto p:mp){
+        //     if(mp[p.first]>0){
+        //         for(int i=W-1;i>=0;i--){
+        //             if((mp[p.first+i]-=mp[p.first])<0) return false;
+        //         }
+        //     }
+        // }
+        // return true;
+    }
 };
 
 class JuneDailyProblems: public Solution{
@@ -188,6 +225,7 @@ class JuneDailyProblems: public Solution{
             cout << "Day 3: Append Characters to String to Make Subsequence\n";
             cout << "Day 4: Longest Palindrome\n";
             cout << "Day 5: Find Common Characters\n";
+            cout << "Day 6: Hand of Straights\n";
             cout << "88: Exit" << '\n';
             int day; cin >> day;
             switch(day){
@@ -229,6 +267,17 @@ class JuneDailyProblems: public Solution{
                     cout << "The common characters in the words are : ";
                     for(auto c:ans) cout << c << " ";
                     cout << '\n';
+                    break;
+                }
+                case 6:{
+                    cout << "Enter the number of cards\n";
+                    int n; cin >> n;
+                    vector<int> cards(n);
+                    cout << "Enter the cards\n";
+                    for(int i=0;i<n;i++) cin >> cards[i];
+                    cout << "Enter the number of cards in each group\n";
+                    int w; cin >> w;
+                    cout << "Is it possible to rearrange the cards into groups of size " << w << " : " << s.isNStraightHand(cards,w) << '\n';
                     break;
                 }
                 case 88:{
