f41bda25287c061c6a15d87e832f64952825cbdf
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-06-07 07:22:46 -0400

    Updating comments and printing statement in JuneDailyProblems cpp file.

--- leetcode/June 2024/JuneDailyProblems.cpp
+++ leetcode/June 2024/JuneDailyProblems.cpp
@@ -206,6 +206,53 @@ class Solution{
         }
         return true;
     }
+
+
+    // Day 7: Replace Words - Q648(Medium)
+    /*
+        In English, we have a concept called root, which can be followed by some other word to form another longer word - let's call this word successor. 
+        For example, when the root "an" is followed by the successor word "other", we can form a new word "another".
+
+        Given a dictionary consisting of many roots and a sentence consisting of words separated by spaces, replace all the successors in the sentence with the root forming it. 
+        If a successor can be replaced by more than one root, replace it with the root that has the shortest length.
+
+        Return the sentence after the replacement.
+
+        Example 1:
+        Input: dictionary = ["cat","bat","rat"], sentence = "the cattle was rattled by the battery"
+        Output: "the cat was rat by the bat"
+        
+        Example 2:
+        Input: dictionary = ["a","b","c"], sentence = "aadsfasf absbs bbab cadsfafs"
+        Output: "a a b c"
+        
+        Example 3:
+        Input: dictionary = ["a", "aa", "aaa", "aaaa"], sentence = "a aa a aaaa aaa aaa aaa aaaaaa bbb baba ababa"
+        Output: "a a a a a a a a bbb baba a"
+        
+        Example 4:
+        Input: dictionary = ["catt","cat","bat","rat"], sentence = "the cattle was rattled by the battery"
+        Output: "the cat was rat by the bat"
+        
+        Example 5:
+        Input: dictionary = ["ac","ab"], sentence = "it is abnormal that this solution is accepted"
+        Output: "it is ab that this solution is ac"
+        
+
+        Constraints:
+        1 <= dictionary.length <= 1000
+        1 <= dictionary[i].length <= 100
+        dictionary[i] consists of only lower-case letters.
+        1 <= sentence.length <= 10^6
+        sentence consists of only lower-case letters and spaces.
+        The number of words in sentence is in the range [1, 1000]
+        The length of each word in sentence is in the range [1, 1000]
+        Each two consecutive words in sentence will be separated by exactly one space.
+        sentence does not have leading or trailing spaces.
+    */
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        
+    }
 };
 
 class JuneDailyProblems: public Solution{
@@ -226,6 +273,7 @@ class JuneDailyProblems: public Solution{
             cout << "Day 4: Longest Palindrome\n";
             cout << "Day 5: Find Common Characters\n";
             cout << "Day 6: Hand of Straights\n";
+            cout << "Day 7: Replace Words\n";
             cout << "88: Exit" << '\n';
             int day; cin >> day;
             switch(day){
@@ -278,6 +326,17 @@ class JuneDailyProblems: public Solution{
                     cout << "Enter the number of cards in each group\n";
                     int w; cin >> w;
                     cout << "Is it possible to rearrange the cards into groups of size " << w << " : " << (bool)s.isNStraightHand(cards,w) << '\n';
+                    break;
+                }
+                case 7:{
+                    cout << "Enter the number of words in the dictionary\n";
+                    int n; cin >> n;
+                    vector<string> dictionary(n);
+                    cout << "Enter the words in the dictionary\n";
+                    for(int i=0;i<n;i++) cin >> dictionary[i];
+                    cout << "Enter the sentence\n";
+                    string sentence; cin >> sentence;
+                    cout << "The sentence after the replacement is : " << s.replaceWords(dictionary,sentence) << '\n';
                     break;
                 }
                 case 88:{
