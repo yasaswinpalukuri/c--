035ac702fc0ded85907c7c11f08d7170602b586c
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-06-09 19:42:50 -0400

    Added solution and comments to the java file for Day 9

--- leetcode/June 2024/JuneDailyProblems.java
+++ leetcode/June 2024/JuneDailyProblems.java
@@ -509,19 +509,19 @@ class Solution {
         2 <= k <= 104
     */
     public int subarraysDivByK(int[] nums, int k) {
-        // Map<Integer, Integer> map = new HashMap<>();
-        // map.put(0, 1);
-        // int sum = 0;
-        // int count = 0;
-        // for (int num : nums) {
-        //     sum = (sum + num) % k;
-        //     if (sum < 0) {
-        //         sum += k;
-        //     }
-        //     count += map.getOrDefault(sum, 0);
-        //     map.put(sum, map.getOrDefault(sum, 0) + 1);
-        // }
-        // return count;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+        int count = 0;
+        for (int num : nums) {
+            sum = (sum + num) % k;
+            if (sum < 0) {
+                sum += k;
+            }
+            count += map.getOrDefault(sum, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
     }
 }
 
