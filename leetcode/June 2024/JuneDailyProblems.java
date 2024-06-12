b55a094a563357586e8c661c16f885154255b8b1
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-06-12 08:11:07 -0400

    Updating comments and printing statement in JuneDailyProblems java file.

--- leetcode/June 2024/JuneDailyProblems.java
+++ leetcode/June 2024/JuneDailyProblems.java
@@ -24,6 +24,7 @@ public class JuneDailyProblems extends Solution {
             System.out.println("Day 9: Subarray Sums Divisible by K");
             System.out.println("Day 10: Height Checker");
             System.out.println("Day 11: Relative Sort Array");
+            System.out.println("Day 12: Sort Colors");
             System.out.println("88: Exit");
             System.out.print("Enter the day of the problem you want the answer for:");
             int day = scan.nextInt();
@@ -131,6 +132,16 @@ public class JuneDailyProblems extends Solution {
                     System.out.println("Enter the elements of the second array");
                     for(int i = 0; i < m; i++) arr2[i] = scan.nextInt();
                     System.out.println("The relative sort array is: " + Arrays.toString(s.relativeSortArray(arr1, arr2)));
+                    break;
+                }
+                case 12:{
+                    System.out.println("Enter the number of elements in the array");
+                    int n = scan.nextInt();
+                    int[] nums = new int[n];
+                    System.out.println("Enter the elements of the array");
+                    for(int i = 0; i < n; i++) nums[i] = scan.nextInt();
+                    s.sortColors(nums);
+                    System.out.println("The sorted array is: " + Arrays.toString(nums));
                     break;
                 }
                 case 88: {
@@ -656,6 +667,38 @@ class Solution {
         }
         return ans;
     }
+
+
+    // Day 12: Sort Colors - Q75(Medium)
+    /*
+        Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, 
+        with the colors in the order red, white, and blue.
+        We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+        You must solve this problem without using the library's sort function.
+
+        Example 1:
+        Input: nums = [2,0,2,1,1,0]
+        Output: [0,0,1,1,2,2]
+        
+        Example 2:
+        Input: nums = [2,0,1]
+        Output: [0,1,2]
+        
+        Example 3:
+        Input: nums = [0]
+        Output: [0]
+        
+        Example 4:
+        Input: nums = [1]
+        Output: [1]
+        
+
+        Constraints:
+        n == nums.length
+        1 <= n <= 300
+        nums[i] is 0, 1, or 2.
+    */
 }
 
 
