1ec4759fde1ae14039f56a3b6763887ffa3c46bf
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-06-12 08:16:16 -0400

    Added solution and comments to the python file for Day 12

--- leetcode/June 2024/JuneDailyProblem.py
+++ leetcode/June 2024/JuneDailyProblem.py
@@ -469,17 +469,17 @@ class Solutions:
         nums[i] is 0, 1, or 2.
     '''
     def sortColors(self, nums: list) -> None:
-        # low, mid, high = 0, 0, len(nums) - 1
-        # while mid <= high:
-        #     if nums[mid] == 0:
-        #         nums[low], nums[mid] = nums[mid], nums[low]
-        #         low += 1
-        #         mid += 1
-        #     elif nums[mid] == 1:
-        #         mid += 1
-        #     else:
-        #         nums[mid], nums[high] = nums[high], nums[mid]
-        #         high -= 1
+        low, mid, high = 0, 0, len(nums) - 1
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
 
 
 class JuneDailyProblems(Solutions):
