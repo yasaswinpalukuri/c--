c49000afd69c2ec348366c7c8d22a5dde0d99152
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-06-13 11:26:53 -0400

    Updating comments and printing statement in JuneDailyProblems cpp file.

--- leetcode/June 2024/JuneDailyProblems.cpp
+++ leetcode/June 2024/JuneDailyProblems.cpp
@@ -480,6 +480,54 @@ class Solution{
             else if(nums[i]==2) swap(nums[i--],nums[high--]);
         }
     }
+
+
+    // Day 13: Minimum Number of Moves to Seat Everyone - Q2037(Easy)
+    /*
+        There are n seats and n students in a room. You are given an array seats of length n, where seats[i] is the position of the ith seat. 
+        You are also given the array students of length n, where students[j] is the position of the jth student.
+
+        You may perform the following move any number of times:
+
+        Increase or decrease the position of the ith student by 1 (i.e., moving the ith student from position x to x + 1 or x - 1)
+        Return the minimum number of moves required to move each student to a seat such that no two students are in the same seat.
+
+        Note that there may be multiple seats or students in the same position at the beginning.
+
+        Example 1:
+        Input: seats = [3,1,5], students = [2,7,4]
+        Output: 4
+        Explanation: The students are moved as follows:
+        - The first student is moved from from position 2 to position 1 using 1 move.
+        - The second student is moved from from position 7 to position 5 using 2 moves.
+        - The third student is moved from from position 4 to position 3 using 1 move.
+        In total, 1 + 2 + 1 = 4 moves were used.
+        
+        Example 2:
+        Input: seats = [4,1,5,9], students = [1,3,2,6]
+        Output: 7
+        Explanation: The students are moved as follows:
+        - The first student is not moved.
+        - The second student is moved from from position 3 to position 4 using 1 move.
+        - The third student is moved from from position 2 to position 1 using 1 move.
+        - The fourth student is moved from from position 6 to position 9 using 3 moves.
+        In total, 0 + 1 + 1 + 3 = 5 moves were used.
+        
+        Example 3:
+        Input: seats = [2,2,6], students = [1,3,2]
+        Output: 4
+        Explanation: The students are moved as follows:
+        - The first student is moved from from position 1 to position 2 using 1 move.
+        - The second student is not moved.
+        - The third student is moved from from position 2 to position 6 using 3 moves.
+    */
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        // sort(seats.begin(),seats.end());
+        // sort(students.begin(),students.end());
+        // int ans = 0;
+        // for(int i=0;i<seats.size();i++) ans += abs(seats[i]-students[i]);
+        // return ans;
+    }
 };
 
 class JuneDailyProblems: public Solution{
@@ -506,6 +554,7 @@ class JuneDailyProblems: public Solution{
             cout << "Day 10: Height Checker\n";
             cout << "Day 11: Relative Sort Array\n";
             cout << "Day 12: Sort Colors\n";
+            cout << "Day 13: Minimum Number of Moves to Seat Everyone\n";
             cout << "88: Exit" << '\n';
             int day; cin >> day;
             switch(day){
@@ -629,6 +678,20 @@ class JuneDailyProblems: public Solution{
                     cout << "The sorted array is : ";
                     for(auto c:nums) cout << c << " ";
                     cout << '\n';
+                    break;
+                }
+                case 13:{
+                    cout << "Enter the number of seats\n";
+                    int n; cin >> n;
+                    vector<int> seats(n);
+                    cout << "Enter the positions of the seats\n";
+                    for(int i=0;i<n;i++) cin >> seats[i];
+                    cout << "Enter the number of students\n";
+                    int m; cin >> m;
+                    vector<int> students(m);
+                    cout << "Enter the positions of the students\n";
+                    for(int i=0;i<m;i++) cin >> students[i];
+                    cout << "The minimum number of moves required to move each student to a seat such that no two students are in the same seat is : " << s.minMovesToSeat(seats,students) << '\n';
                     break;
                 }
                 case 88:{
