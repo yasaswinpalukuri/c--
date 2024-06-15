1e8e52cc51255aab795cb44baac58a6d37b51362
Yasaswin <palukuriyasaswinkumar@gmail.com>
2024-06-14 22:31:49 -0400

    Added solution and comments to the java file for Day 14

--- leetcode/June 2024/JuneDailyProblems.java
+++ leetcode/June 2024/JuneDailyProblems.java
@@ -812,21 +812,21 @@ class Solution {
         0 <= nums[i] <= 105
     */
     public int minIncrementForUnique(int[] nums) {
-        // int[] count = new int[100000];
-        // for (int x : nums) {
-        //     count[x]++;
-        // }
-        // int ans = 0, taken = 0;
-        // for (int x = 0; x < 100000; ++x) {
-        //     if (count[x] >= 2) {
-        //         taken += count[x] - 1;
-        //         ans -= x * (count[x] - 1);
-        //     } else if (taken > 0 && count[x] == 0) {
-        //         taken--;
-        //         ans += x;
-        //     }
-        // }
-        // return ans;
+        int[] count = new int[100000];
+        for (int x : nums) {
+            count[x]++;
+        }
+        int ans = 0, taken = 0;
+        for (int x = 0; x < 100000; ++x) {
+            if (count[x] >= 2) {
+                taken += count[x] - 1;
+                ans -= x * (count[x] - 1);
+            } else if (taken > 0 && count[x] == 0) {
+                taken--;
+                ans += x;
+            }
+        }
+        return ans;
     }
 }
 
